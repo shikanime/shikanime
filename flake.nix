0c732b4105a913b1774ef2a1706e8d59c3ab6837
Shikanime Deva <deva.shikanime@protonmail.com>
2021-12-09 22:56:43 +0100

    Add Nix home configuration

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Shikanime's home configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/release-21.11";
+    home-manager.url = "github:nix-community/home-manager/release-21.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }: {
+    homeConfigurations = {
+      altashar = home-manager.lib.homeManagerConfiguration {
+        system = "x86_64-darwin";
+        homeDirectory = "/Users/williamphetsinorath";
+        username = "williamphetsinorath";
+        stateVersion = "21.11";
+        configuration = { config, pkgs, ... }: {
+          nixpkgs.config.allowUnfree = true;
+          imports = [ ./home.nix ];
+        };
+      };
+      ishtar = home-manager.lib.homeManagerConfiguration {
+        system = "x86_64-linux";
+        homeDirectory = "/home/devas";
+        username = "devas";
+        stateVersion = "21.11";
+        configuration = { config, pkgs, ... }: {
+          nixpkgs.config.allowUnfree = true;
+          imports = [ ./home.nix ];
+        };
+      };
+    };
+    defaultPackage.x86_64-darwin =
+      self.homeConfigurations.altashar.activationPackage;
+    defaultPackage.x86_64-linux =
+      self.homeConfigurations.ishtar.activationPackage;
+  };
+}
---
+++ home.nix
@@ -0,0 +1,193 @@
+{ lib, config, pkgs ? import <nixpkgs> { }, ... }:
+
+let
+  devPackages = [
+    pkgs.yarn
+    pkgs.darcs
+    pkgs.nixfmt
+    pkgs.rustup
+    pkgs.poetry
+    pkgs.python3Packages.black
+    pkgs.python3Packages.flake8
+    pkgs.python3Packages.autopep8
+    pkgs.nodePackages.esy
+    pkgs.nodePackages.eslint
+    pkgs.nodePackages.stylelint
+    pkgs.nodePackages.htmlhint
+    pkgs.nodePackages.jshint
+    pkgs.nodePackages.prettier
+  ];
+
+  cloudPackages = [
+    pkgs.google-cloud-sdk
+    pkgs.azure-cli
+    pkgs.aws
+    pkgs.kubectl
+    pkgs.minikube
+    pkgs.cloudflared
+    pkgs.terraform
+    pkgs.github-cli
+    pkgs.podman
+  ];
+
+  utilityPackages = [ pkgs.openssh pkgs.unzip pkgs.htop pkgs.yq pkgs.zip ]
+    ++ lib.optionals pkgs.stdenv.hostPlatform.isLinux [
+      pkgs.inotify-tools
+      pkgs.usbutils
+    ];
+in {
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # Session configuration
+  home.sessionVariables.EDITOR = "vim";
+
+  # Core global utilitary packages
+  home.packages = devPackages ++ cloudPackages ++ utilityPackages;
+
+  home.file.".editorconfig".text = ''
+    # top-most EditorConfig file
+    root = true
+
+    # Unix-style newlines with a newline ending every file
+    [*]
+    end_of_line = lf
+    insert_final_newline = true
+
+    # Matches multiple files with brace expansion notation
+    # Set default charset
+    [*.{js,py}]
+    charset = utf-8
+
+    # 4 space indentation
+    [*.py]
+    indent_style = space
+    indent_size = 4
+
+    # Tab indentation (no size specified)
+    [Makefile]
+    indent_style = tab
+  '';
+
+  programs.java.enable = true;
+
+  # TODO: find a way to add gopls, gopkgs, go-outline, dlv, dlv-dap and staticcheck.
+  programs.go.enable = true;
+
+  programs.vim.enable = true;
+
+  programs.opam = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+  programs.jq.enable = true;
+
+  programs.zsh = {
+    enable = true;
+    autocd = true;
+    enableAutosuggestions = true;
+    enableCompletion = true;
+    enableSyntaxHighlighting = true;
+    oh-my-zsh = {
+      enable = true;
+      plugins = [
+        "git"
+        "ubuntu"
+        "gcloud"
+        "aws"
+        "python"
+        "docker"
+        "kubectl"
+        "rust"
+        "rustup"
+        "node"
+        "minikube"
+        "golang"
+        "sudo"
+        "vim-interaction"
+      ];
+    };
+  };
+
+  programs.direnv = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+  programs.starship = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+  programs.tmux = {
+    enable = true;
+    newSession = true;
+  };
+
+  programs.gpg.enable = true;
+
+  services.gpg-agent = lib.mkIf pkgs.stdenv.hostPlatform.isLinux {
+    enable = true;
+    enableSshSupport = true;
+  };
+
+  programs.ssh = {
+    enable = true;
+    matchBlocks = {
+      "gitlab.com" = {
+        hostname = "gitlab.com";
+        identityFile = "${config.home.homeDirectory}/.ssh/gitlab";
+        identitiesOnly = true;
+      };
+      "github.com" = {
+        hostname = "github.com";
+        identityFile = "${config.home.homeDirectory}/.ssh/github";
+        identitiesOnly = true;
+      };
+      "ssh.dev.azure.com" = {
+        hostname = "ssh.dev.azure.com";
+        identityFile = "${config.home.homeDirectory}/.ssh/azure";
+        identitiesOnly = true;
+      };
+      "gcmd.birdz.com" = {
+        hostname = "gcmd.birdz.com";
+        identityFile = "${config.home.homeDirectory}/.ssh/birdz";
+        identitiesOnly = true;
+      };
+    };
+    extraConfig = ''
+      HostKeyAlgorithms +ssh-rsa
+      PubkeyAcceptedKeyTypes +ssh-rsa
+    '';
+  };
+
+  programs.mercurial = {
+    enable = true;
+    userName = "Shikanime Deva";
+    userEmail = "deva.shikanime@protonmail.com";
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "Shikanime Deva";
+    userEmail = "deva.shikanime@protonmail.com";
+
+    aliases.adog = "log --all --decorate --oneline --graph";
+    ignores = [ "*~" ".fuse_hidden*" ".directory" ".Trash-*" ".nfs*" ];
+
+    lfs.enable = true;
+
+    extraConfig = {
+      core.editor = "vim";
+      color.ui = "auto";
+      pull = {
+        ff = "only";
+        rebase = true;
+      };
+      rebase.autoStash = true;
+      init.defaultBranch = "main";
+      credential."https://dev.azure.com".useHttpPath = true;
+    };
+  };
+}
