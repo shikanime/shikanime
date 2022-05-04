d164e2ea2377e6541689d21b86c1961dfaf3aef9
Shikanime Deva <shikanime.deva@shikanime.studio>
2022-05-04 10:52:27 +0200

    Add Virtualbox share file group

--- nixos/modules/configuration.nix
+++ nixos/modules/configuration.nix
@@ -5,7 +5,7 @@
   users.users.devas = {
     isNormalUser = true;
     home = "/home/devas";
-    extraGroups = [ "docker" "wheel" "kvm" ];
+    extraGroups = [ "docker" "wheel" "kvm" "vboxsf" ];
     shell = pkgs.zsh;
     hashedPassword = "$6$YS5jCyZU2Z6i05wm$jFsx9fnINawEk2Vd5uZBdR71sOBHHgANUEBsp93fG3scp2uui3kYhzXh9c4eC4ZdHKq48//IWE00JwZ.ez.lg.";
     openssh.authorizedKeys.keys = [
