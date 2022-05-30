6809b457ef3cd80fa4b47c50bbab8ba7d1cba3ed
Shikanime Deva <shikanime.deva@shikanime.studio>
2022-05-30 18:50:36 +0200

    Revert dev tools to host

--- home/default.nix
+++ home/default.nix
@@ -7,14 +7,13 @@
   # Let Home Manager install and manage itself
   programs.home-manager.enable = true;
 
+  # Core global utilitary packages
+  home.packages = [
+    pkgs.rustup
+  ];
+
   # Session configuration
   home.sessionVariables.EDITOR = "vim";
-
-  programs.direnv = {
-    enable = true;
-    enableBashIntegration = true;
-    enableZshIntegration = true;
-  };
 
   programs.vim.enable = true;
 
@@ -30,6 +29,18 @@
     enableZshIntegration = true;
   };
 
+  programs.opam = {
+    enable = true;
+    enableBashIntegration = true;
+    enableZshIntegration = true;
+  };
+
+  programs.direnv = {
+    enable = true;
+    enableBashIntegration = true;
+    enableZshIntegration = true;
+  };
+
   programs.zsh = {
     enable = true;
     autocd = true;
@@ -40,7 +51,17 @@
       enable = true;
       plugins = [
         "git"
+        "gcloud"
+        "aws"
+        "python"
+        "docker"
+        "kubectl"
+        "rust"
+        "node"
+        "minikube"
+        "golang"
         "sudo"
+        "yarn"
         "vim-interaction"
       ];
     };
@@ -49,6 +70,22 @@
   programs.ssh = {
     enable = true;
     matchBlocks = {
+      "coopelec.ssh.dev.azure.com" = {
+        hostname = "ssh.dev.azure.com";
+        identityFile = "~/.ssh/coopelec_rsa";
+      };
+      "lvmh.ssh.dev.azure.com" = {
+        hostname = "ssh.dev.azure.com";
+        identityFile = "~/.ssh/lvmh_ed25519";
+      };
+      "gcmd.birdz.com" = {
+        hostname = "gcmd.birdz.com";
+        identityFile = "~/.ssh/birdz_ed25519";
+      };
+      "sfeir.gitlab.com" = {
+        hostname = "gitlab.com";
+        identityFile = "~/.ssh/sfeir_ed25519";
+      };
       "elkia.local" = {
         hostname = "elkia.local";
         identityFile = "~/.ssh/elkia";
@@ -64,5 +101,56 @@
         forwardAgent = true;
       };
     };
+    extraConfig = ''
+      IdentitiesOnly yes
+      HostKeyAlgorithms +ssh-rsa
+      PubkeyAcceptedKeyTypes +ssh-rsa
+    '';
+  };
+
+  programs.mercurial = {
+    enable = true;
+    userName = "Shikanime Deva";
+    userEmail = "shikanime.deva@shikanime.studio";
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "Shikanime Deva";
+    userEmail = "shikanime.deva@shikanime.studio";
+
+    aliases = {
+      adog = "log --all --decorate --oneline --graph";
+      pouf = "push --force-with-lease";
+    };
+    ignores = [
+      "*~"
+      ".fuse_hidden*"
+      ".directory"
+      ".Trash-*"
+      ".nfs*"
+    ];
+
+    lfs.enable = true;
+
+    extraConfig = {
+      core.editor = "vim";
+      color.ui = "auto";
+      pull.rebase = true;
+      rebase.autostash = true;
+      init.defaultBranch = "main";
+      credential."https://dev.azure.com".useHttpPath = true;
+      credential."https://source.developers.google.com".helper = "gcloud.sh";
+      credential.helper = "store";
+      user.signingKey = "B9443725856FF9EB";
+      commit.gpgSign = true;
+    };
+  };
+
+  programs.gpg.enable = true;
+
+  services.gpg-agent = {
+    enable = true;
+    enableSshSupport = true;
   };
 }
