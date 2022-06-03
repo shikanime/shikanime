5d85c974eb68e58496b65f315d315f70924597af
Shikanime Deva <shikanime.deva@shikanime.studio>
2022-06-03 20:55:41 +0000

    Add JetBrains patcher

--- modules/compat.nix
+++ modules/compat.nix
@@ -3,7 +3,6 @@
 {
   systemd.user.services.vscode-server-patcher = {
     description = "Automatically fix the VS Code server used by the remote SSH extension";
-
     serviceConfig = {
       Restart = "always";
       ExecStart = pkgs.writeShellScript "patch-vscode-server" ''
@@ -25,7 +24,34 @@
         done
       '';
     };
+    wantedBy = [ "default.target" ];
+  };
 
+  systemd.user.services.jetbrains-patcher = {
+    description = "Automatically fix the JetBrains used by the Remote Dev";
+    serviceConfig = {
+      Restart = "always";
+      ExecStart = pkgs.writeShellScript "patch-jetbrains" ''
+        ${pkgs.inotify-tools}/bin/inotifywait \
+          --monitor \
+          --event delete \
+          --format "%w" \
+          "$HOME/.cache/JetBrains/RemoteDev/dist" \
+          | while IFS=: read -r out;
+        do
+          find $out/*/plugins/remote-dev-server/bin \
+            -mindepth 1 \
+            -maxdepth 1 \
+            -name launcher.sh \
+            -type f \
+            | while IFS=: read -r file;
+          do
+            sed -i 's#/lib64/ld-linux-x86-64.so.2#${pkgs.glibc.out}/lib/ld-linux-x86-64.so.2#g' \
+              "$file"
+          done
+        done
+      '';
+    };
     wantedBy = [ "default.target" ];
   };
 }
