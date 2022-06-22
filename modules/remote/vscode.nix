a307056c5789680a2ea9565fb673a11a1e514385
Shikanime Deva <shikanime.deva@shikanime.studio>
2022-06-22 11:08:46 +0000

    Add VSCode extensions patcher

--- modules/remote/vscode.nix
+++ modules/remote/vscode.nix
@@ -28,4 +28,30 @@ with lib;
     };
     wantedBy = [ "default.target" ];
   };
+
+  systemd.user.services.vscode-server-extensions-patcher = {
+    description = "Automatically fix the VS Code server extensions used by the remote SSH extension";
+    unitConfig.ConditionPathIsDirectory = "%h/.vscode-server/extensions";
+    serviceConfig = {
+      Restart = "always";
+      ExecStart = pkgs.writeShellScript "patch-vscode-server" ''
+        ${pkgs.inotify-tools}/bin/inotifywait \
+          --monitor \
+          --recursive \
+          --event create \
+          --event moved_to \
+          "$HOME/.vscode-server/extensions" \
+          | while IFS=:" " read -r out event filename;
+        do
+          if ${pkgs.file}/bin/file "$out$filename" | grep -q ELF; then
+            ${pkgs.patchelf}/bin/patchelf \
+              --set-interpreter "${pkgs.glibc.out}/lib/ld-linux-x86-64.so.2" \
+              --set-rpath "${makeLibraryPath [pkgs.stdenv.cc.cc.lib]}" \
+              "$out$filename"
+          fi
+        done
+      '';
+    };
+    wantedBy = [ "default.target" ];
+  };
 }
