add53ac9bfd528a42e2d514bc392012866cb89a2
Shikanime Deva <shikanime.deva@shikanime.studio>
2022-09-20 14:33:40 +0200

    Fix service crash on first boot

--- modules/remote/vscode.nix
+++ modules/remote/vscode.nix
@@ -5,70 +5,56 @@ with lib;
 {
   systemd.user.services.vscode-server-patcher = {
     description = "Automatically fix the VS Code server used by the remote SSH extension";
-    serviceConfig = {
-      Restart = "always";
-      ExecStart = pkgs.writeShellScript "patch-vscode-server" ''
-        ${pkgs.inotify-tools}/bin/inotifywait \
-          --monitor \
-          --recursive \
-          --event create \
-          --event moved_to \
-          "$HOME/.vscode-server/bin" \
-          | while IFS=:" " read -r out event filename;
-        do
-          if ${pkgs.file}/bin/file "$out$filename" | grep -q ELF; then
-            ${pkgs.patchelf}/bin/patchelf \
-              --set-interpreter "${pkgs.glibc.out}/lib/ld-linux-x86-64.so.2" \
-              --set-rpath "${makeLibraryPath [pkgs.stdenv.cc.cc.lib]}" \
-              "$out$filename"
-          fi
-        done
-      '';
-    };
-    wantedBy = [ "default.target" ];
+    script = ''
+      ${pkgs.inotify-tools}/bin/inotifywait \
+        --monitor \
+        --recursive \
+        --event create \
+        --event moved_to \
+        "$HOME/.vscode-server/bin" \
+        | while IFS=:" " read -r out event filename;
+      do
+        if ${pkgs.file}/bin/file "$out$filename" | grep -q ELF; then
+          ${pkgs.patchelf}/bin/patchelf \
+            --set-interpreter "${pkgs.glibc}/lib/ld-linux-x86-64.so.2" \
+            --set-rpath "${makeLibraryPath [pkgs.stdenv.cc.cc.lib]}" \
+            "$out$filename"
+        fi
+      done
+    '';
   };
 
   systemd.user.paths.vscode-server-patcher = {
     description = "Monitor VS Code server connection";
-    pathConfig = {
-      PathExists = "%h/.vscode-server/bin";
-      Unit = "vscode-server-patcher.service";
-    };
-    wantedBy = [ "default.target" ];
+    pathConfig.PathExists = "%h/.vscode-server/bin";
+    wantedBy = [ "multi-user.target" ];
   };
 
   systemd.user.services.vscode-server-extensions-patcher = {
     description = "Automatically fix the VS Code server extensions used by the remote SSH extension";
-    serviceConfig = {
-      Restart = "always";
-      ExecStart = pkgs.writeShellScript "patch-vscode-server-extensions" ''
-        ${pkgs.inotify-tools}/bin/inotifywait \
-          --monitor \
-          --recursive \
-          --event create \
-          --event moved_to \
-          --event modify \
-          "$HOME/.vscode-server/extensions" \
-          | while IFS=:" " read -r out event filename;
-        do
-          if ${pkgs.file}/bin/file "$out$filename" | grep -q ELF; then
-            ${pkgs.patchelf}/bin/patchelf \
-              --set-interpreter "${pkgs.glibc.out}/lib/ld-linux-x86-64.so.2" \
-              --set-rpath "${makeLibraryPath [pkgs.stdenv.cc.cc.lib pkgs.zlib pkgs.zlib]}:${pkgs.openjdk}/lib/openjdk/lib" \
-              "$out$filename"
-          fi
-        done
-      '';
-    };
-    wantedBy = [ "default.target" ];
+    script = ''
+      ${pkgs.inotify-tools}/bin/inotifywait \
+        --monitor \
+        --recursive \
+        --event create \
+        --event moved_to \
+        --event modify \
+        "$HOME/.vscode-server/extensions" \
+        | while IFS=:" " read -r out event filename;
+      do
+        if ${pkgs.file}/bin/file "$out$filename" | grep -q ELF; then
+          ${pkgs.patchelf}/bin/patchelf \
+            --set-interpreter "${pkgs.glibc}/lib/ld-linux-x86-64.so.2" \
+            --set-rpath "${makeLibraryPath [pkgs.stdenv.cc.cc.lib pkgs.zlib pkgs.zlib]}:${pkgs.openjdk}/lib/openjdk/lib" \
+            "$out$filename"
+        fi
+      done
+    '';
   };
 
   systemd.user.paths.vscode-server-extensions-patcher = {
     description = "Monitor VS Code server extensions connection";
-    pathConfig = {
-      PathExists = "%h/.vscode-server/extensions";
-      Unit = "vscode-server-extensions-patcher.service";
-    };
-    wantedBy = [ "default.target" ];
+    pathConfig.PathExists = "%h/.vscode-server/extensions";
+    wantedBy = [ "multi-user.target" ];
   };
 }
