1d7e8547bb1d4435e1b7fc451587bcb384979837
Shikanime Deva <shikanime.deva@shikanime.studio>
2022-09-20 14:33:40 +0200

    Fix service crash on first boot

--- modules/remote/jetbrains.nix
+++ modules/remote/jetbrains.nix
@@ -3,30 +3,23 @@
 {
   systemd.user.services.jetbrains-remote-development-patcher = {
     description = "Automatically fix the JetBrains Remote Development used by the Remote Dev";
-    serviceConfig = {
-      Restart = "always";
-      ExecStart = pkgs.writeShellScript "patch-jetbrains-remote-development" ''
-        ${pkgs.inotify-tools}/bin/inotifywait \
-          --monitor \
-          --event delete \
-          --include ".tar.gz" \
-          "$HOME/.cache/JetBrains/RemoteDev/dist" \
-          | while IFS=:" " read -r out event filename;
-        do
-          sed -i 's#/lib64/ld-linux-x86-64.so.2#${pkgs.glibc.out}/lib/ld-linux-x86-64.so.2#g' \
-            "$out''${filename%%.tar.gz}/plugins/remote-dev-server/bin/launcher.sh"
-        done
-      '';
-    };
-    wantedBy = [ "default.target" ];
+    script = ''
+      ${pkgs.inotify-tools}/bin/inotifywait \
+        --monitor \
+        --event delete \
+        --include ".tar.gz" \
+        "$HOME/.cache/JetBrains/RemoteDev/dist" \
+        | while IFS=:" " read -r out event filename;
+      do
+        sed -i 's#/lib64/ld-linux-x86-64.so.2#${pkgs.glibc}/lib/ld-linux-x86-64.so.2#g' \
+          "$out''${filename%%.tar.gz}/plugins/remote-dev-server/bin/launcher.sh"
+      done
+    '';
   };
 
   systemd.user.paths.jetbrains-remote-development-patcher = {
     description = "Monitor JetBrains Remote Development connection";
-    pathConfig = {
-      PathExists = "%h/.cache/JetBrains/RemoteDev/dist";
-      Unit = "jetbrains-remote-development-patcher.service";
-    };
-    wantedBy = [ "default.target" ];
+    pathConfig.PathExists = "%h/.cache/JetBrains/RemoteDev/dist";
+    wantedBy = [ "multi-user.target" ];
   };
 }
