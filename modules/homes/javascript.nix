28980ab1fb9a627954a5a59733dae2c38a9badc0
Shikanime Deva <shikanime.deva@shikanime.studio>
2022-09-21 16:46:26 +0200

    Fix missing NPM cache dir

--- modules/homes/javascript.nix
+++ modules/homes/javascript.nix
@@ -9,9 +9,6 @@
 
   home.sessionVariables.NPM_CONFIG_USERCONFIG = "${config.xdg.configHome}/npm/config";
 
-  home.file.".npmrc".source = config.lib.file.mkOutOfStoreSymlink ".config/npm/config";
-  home.file.".npm".source = config.lib.file.mkOutOfStoreSymlink "${config.xdg.dataHome}/npm";
-
   programs.zsh.oh-my-zsh.plugins = [
     "deno"
     "node"
