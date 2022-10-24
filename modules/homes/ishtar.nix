33c32ce08a14db2cc65feb07ab6850895b931979
William Phetsinorath <william.phetsinorath@shikanime.studio>
2022-10-24 21:11:15 +0200

    Source host system conda if exist

--- modules/homes/ishtar.nix
+++ modules/homes/ishtar.nix
@@ -1,6 +1,15 @@
+{ config, ... }:
+
 {
   xdg.userDirs = {
     enable = true;
     createDirectories = true;
   };
+
+  programs.zsh.initExtra = ''
+    # Source Anaconda3 if it exists
+    if [ -d ${config.xdg.dataHome}/anaconda3 ]; then
+      source ${config.xdg.dataHome}/anaconda3/bin/activate
+    fi
+  '';
 }
