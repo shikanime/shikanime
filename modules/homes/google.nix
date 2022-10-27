35c7c525ca28d0060b95ee93bcbdbcf99709d215
William Phetsinorath <william.phetsinorath@shikanime.studio>
2022-10-27 13:01:06 +0200

    Use new GKE auth module

--- modules/homes/google.nix
+++ modules/homes/google.nix
@@ -5,6 +5,9 @@
     pkgs.google-cloud-sdk
   ];
 
+  # GCP auth plugin is deprecated in v1.22+, unavailable in v1.26+
+  home.sessionVariables.USE_GKE_GCLOUD_AUTH_PLUGIN = "True";
+
   programs.zsh.oh-my-zsh.plugins = [
     "gcloud"
   ];
