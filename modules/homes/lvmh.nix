13b414a73d49ec4beb5407ce3208511a4a3e82f1
William Phetsinorath <william.phetsinorath@shikanime.studio>
2022-11-24 14:01:52 +0100

    Migrate LVMH config

--- modules/homes/lvmh.nix
+++ modules/homes/lvmh.nix
@@ -1,10 +1,28 @@
-{
-  programs.ssh.matchBlocks = {
-    "lvmh.ssh.dev.azure.com" = {
-      hostname = "ssh.dev.azure.com";
-      identityFile = "~/.ssh/lvmh_rsa";
-    };
-  };
+{ config, pkgs, ... }:
 
-  imports = [ ./sfeir.nix ];
+{
+  programs.git.includes = [
+    {
+      condition = "gitdir:${config.home.homeDirectory}/Celine/";
+      contents = {
+        user = {
+          name = "William Phetsinorath";
+          email = "william.phetsinorath-ext@celine.fr";
+          signingKey = "688247A82F19BF0D";
+        };
+        core.sshCommand = "${pkgs.openssh}/bin/ssh -i ${config.home.homeDirectory}/.ssh/lvmh_rsa";
+      };
+    }
+    {
+      condition = "gitdir:${config.home.homeDirectory}/Moynat/";
+      contents = {
+        user = {
+          name = "William Phetsinorath";
+          email = "wphetsinorath_ext@moynat.com";
+          signingKey = "E144E994EA47B1A8";
+        };
+        core.sshCommand = "${pkgs.openssh}/bin/ssh -i ${config.home.homeDirectory}/.ssh/lvmh_rsa";
+      };
+    }
+  ];
 }
