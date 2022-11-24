705c805db858d477b445e907488fbaf11bc156fa
William Phetsinorath <william.phetsinorath@shikanime.studio>
2022-11-24 14:05:18 +0100

    Migrate Birdz

--- modules/homes/birdz.nix
+++ modules/homes/birdz.nix
@@ -1,12 +1,17 @@
 { pkgs, config, ... }:
 
 {
-  programs.ssh.matchBlocks = {
-    "gcmd.birdz.com" = {
-      hostname = "gcmd.birdz.com";
-      identityFile = "~/.ssh/birdz_ed25519";
-    };
-  };
-
-  imports = [ ./sfeir.nix ];
+  programs.git.includes = [
+    {
+      condition = "gitdir:${config.home.homeDirectory}/Birdz/";
+      contents = {
+        user = {
+          name = "William Phetsinorath";
+          email = "phetsinorath.w@sfeir.com";
+          signingKey = "AA83063619D0AAEF";
+        };
+        core.sshCommand = "${pkgs.openssh}/bin/ssh -i ${config.home.homeDirectory}/.ssh/birdz_ed25519";
+      };
+    }
+  ];
 }
