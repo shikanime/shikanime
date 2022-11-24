4eba9989952907b83ee023648bbd95867fb51568
William Phetsinorath <william.phetsinorath@shikanime.studio>
2022-11-24 14:09:27 +0100

    Add tunnel ssh

--- modules/homes/host.nix
+++ modules/homes/host.nix
@@ -21,5 +21,13 @@
       forwardX11Trusted = true;
       forwardAgent = true;
     };
+    "cloudworkstations.dev" = {
+      hostname = "localhost";
+      port = 2222;
+      user = "user";
+      forwardX11 = true;
+      forwardX11Trusted = true;
+      forwardAgent = true;
+    };
   };
 }
