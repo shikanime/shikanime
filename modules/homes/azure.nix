d4ab0c3d4c72a83a29c445c5b7afa77aa5b26e37
William Phetsinorath <william.phetsinorath@shikanime.studio>
2022-12-04 18:53:11 +0100

    Scope SSH config to Azure only

--- modules/homes/azure.nix
+++ modules/homes/azure.nix
@@ -5,11 +5,14 @@
     pkgs.azure-cli
   ];
 
-  programs.ssh.extraConfig = ''
-    IdentitiesOnly yes
-    HostKeyAlgorithms +ssh-rsa
-    PubkeyAcceptedKeyTypes +ssh-rsa
-  '';
-
+  programs.ssh.matchBlocks = {
+    "ssh.dev.azure.com" = {
+      extraOptions = {
+        HostkeyAlgorithms = "+ssh-rsa";
+        PubkeyAcceptedAlgorithms = "+ssh-rsa";
+      };
+    };
+  };
   programs.git.extraConfig.credential."https://dev.azure.com".useHttpPath = true;
 }
+
