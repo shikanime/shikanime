2dc25492bbc36629793dd13ace06252cef6ff9be
William Phetsinorath <william.phetsinorath@shikanime.studio>
2022-12-21 23:55:01 +0100

    Add GPG Agent on Ishtar

--- modules/homes/ishtar.nix
+++ modules/homes/ishtar.nix
@@ -15,4 +15,12 @@
 
   programs.git.extraConfig.credential.helper =
     "/mnt/c/Program\ Files/Git/mingw64/bin/git-credential-manager-core.exe";
+
+  services.gpg-agent = {
+    enable = true;
+    enableSshSupport = true;
+    enableExtraSocket = true;
+    enableBashIntegration = true;
+    enableZshIntegration = true;
+  };
 }
