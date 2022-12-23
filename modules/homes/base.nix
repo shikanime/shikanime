dc8dc0a76563ade4c67d85d487f2dcc6e221e0ad
William Phetsinorath <william.phetsinorath@shikanime.studio>
2022-12-23 19:25:36 +0100

    Add Colemak remap on vim

--- modules/homes/base.nix
+++ modules/homes/base.nix
@@ -100,6 +100,17 @@ with lib;
         vim
       ]))
     ];
+    extraConfig = ''
+      noremap n j|noremap <C-w>n <C-w>j|noremap <C-w><C-n> <C-w>j
+      noremap e k|noremap <C-w>e <C-w>k|noremap <C-w><C-e> <C-w>k
+      noremap s h
+      noremap t l
+
+      noremap f e
+      noremap k n
+      noremap K N
+      noremap U <C-r>
+    '';
   };
 
   programs.gpg.enable = true;
