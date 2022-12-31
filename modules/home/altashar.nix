ae6ce0f39354b7d6c0ef04ed82afd928a0de0311
William Phetsinorath <william.phetsinorath@shikanime.studio>
2022-12-31 18:05:48 +0100

    Refactor Anaconda detection

--- modules/home/altashar.nix
+++ modules/home/altashar.nix
@@ -1,11 +1,4 @@
 {
   home.homeDirectory = "/Users/williamphetsinorath";
   home.username = "williamphetsinorath";
-
-  programs.zsh.initExtra = ''
-    # Source Anaconda3 if it exists
-    if [ -d /usr/local/anaconda3 ]; then
-      source /usr/local/anaconda3/bin/activate
-    fi
-  '';
 }
--- modules/home/ishtar.nix
+++ modules/home/ishtar.nix
@@ -1,17 +1,4 @@
-{ pkgs, config, ... }:
-
 {
   home.homeDirectory = "/home/devas";
   home.username = "devas";
-
-  home.packages = [
-    pkgs.gcc
-  ];
-
-  programs.zsh.initExtra = ''
-    # Source Anaconda3 if it exists
-    if [ -d ${config.xdg.dataHome}/anaconda3 ]; then
-      source ${config.xdg.dataHome}/anaconda3/bin/activate
-    fi
-  '';
 }
