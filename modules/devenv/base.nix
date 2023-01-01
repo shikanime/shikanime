9d6bdab3dfd6ec7cd859dbb84e49340e579f3398
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-01-01 19:59:33 +0100

    Refactor nix devenv module

--- modules/devenv/base.nix
+++ modules/devenv/base.nix
@@ -5,8 +5,12 @@
     actionlint.enable = true;
     markdownlint.enable = true;
     shellcheck.enable = true;
+    nixpkgs-fmt.enable = true;
+    statix.enable = true;
+    deadnix.enable = true;
   };
   packages = [
+    pkgs.nixpkgs-fmt
     pkgs.rubocop
   ];
 }
