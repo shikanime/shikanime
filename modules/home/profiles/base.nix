fd2da72507daba78e192d989bc71695526ef0f92
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-01-17 01:44:24 +0100

    Refactor Vim config to native QWERTY experience

--- modules/home/profiles/base.nix
+++ modules/home/profiles/base.nix
@@ -90,16 +90,40 @@ with lib;
       ]))
     ];
     extraConfig = ''
-      noremap n j|noremap <C-w>n <C-w>j|noremap <C-w><C-n> <C-w>j
-      noremap e k|noremap <C-w>e <C-w>k|noremap <C-w><C-e> <C-w>k
-      noremap s h
-      noremap t l
-
+      noremap d g
+      noremap e k
       noremap f e
+      noremap g t
+      noremap i l
+      noremap j y
       noremap k n
+      noremap l u
+      noremap n j
+      noremap o p
+      noremap p r
+      noremap r s
+      noremap s d
+      noremap t f
+      noremap u i
+      noremap y o
+      noremap D G
+      noremap E K
+      noremap F E
+      noremap G T
+      noremap I L
+      noremap J Y
       noremap K N
-      noremap U <C-r>
-
+      noremap L U
+      noremap N J
+      noremap O P
+      noremap P R
+      noremap R S
+      noremap S D
+      noremap T F
+      noremap U I
+      noremap Y O
+      noremap dd gg
+      noremap df ge
       set number relativenumber
     '';
   };
