e8a8bc5b78c0649dd9e9261897f144f5319b859f
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-01-19 17:16:58 +0100

    Fix Renault key

--- modules/home/users/renault.nix
+++ modules/home/users/renault.nix
@@ -10,7 +10,7 @@
           email = "william.phetsinorath-extern@renault.com";
           signingKey = "AA83063619D0AAEF";
         };
-        core.sshCommand = "${pkgs.openssh}/bin/ssh -i ${config.home.homeDirectory}/.ssh/sfeir_ed25519";
+        core.sshCommand = "${pkgs.openssh}/bin/ssh -i ${config.home.homeDirectory}/.ssh/renault_rsa";
       };
     }
   ];
