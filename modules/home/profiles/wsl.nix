a268ff45069a28b0a3ea03e1499d81d6ee4caca3
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-01-20 01:29:25 +0100

    Use new git cred manager name

--- modules/home/profiles/wsl.nix
+++ modules/home/profiles/wsl.nix
@@ -2,7 +2,7 @@
   targets.genericLinux.enable = true;
 
   programs.git.extraConfig.credential.helper =
-    "/mnt/c/Program\\ Files/Git/mingw64/bin/git-credential-manager-core.exe";
+    "/mnt/c/Program\\ Files/Git/mingw64/bin/git-credential-manager.exe";
 
   # CUDA support
   home.sessionVariables.LD_LIBRARY_PATH = "/usr/lib/wsl/lib";
