33734d81fe0b7277f84eda397a1f24e4d0d5b4e2
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-02-01 23:45:58 +0100

    Lint web nix config

--- modules/home/profiles/web.nix
+++ modules/home/profiles/web.nix
@@ -9,7 +9,7 @@
   programs.zsh.initExtra = ''
     if which brew > /dev/null; then
       if [ -s "$(brew --prefix)/opt/nvm/nvm.sh" ]; then
-      source "$(brew --prefix)/opt/nvm/nvm.sh"
+        source "$(brew --prefix)/opt/nvm/nvm.sh"
       fi
       if [ -s "$(brew --prefix)/opt/nvm/etc/bash_completion.d/nvm" ]; then
         source "$(brew --prefix)/opt/nvm/etc/bash_completion.d/nvm"
