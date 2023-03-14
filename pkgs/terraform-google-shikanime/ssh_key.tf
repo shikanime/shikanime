cd052fa746f71f91834a861ab7d36c98a484c59f
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-03-14 20:31:59 +0100

    Remove Gitlab

--- pkgs/terraform-google-shikanime/ssh_key.tf
+++ pkgs/terraform-google-shikanime/ssh_key.tf
@@ -3,15 +3,3 @@ resource "github_user_ssh_key" "default" {
   title    = title(each.value.name)
   key      = each.value.public_key_openssh
 }
-
-resource "time_rotating" "gitlab_ssh_key_expiration" {
-  rotation_months = 3
-}
-
-resource "gitlab_user_sshkey" "default" {
-  for_each   = var.gitlab.ssh_keys
-  user_id    = data.gitlab_user.default.id
-  title      = title(var.name)
-  key        = each.value.public_key_openssh
-  expires_at = formatdate("YYYY-MM-DD'T'HH:mm:ssZ", time_rotating.gitlab_ssh_key_expiration.rotation_rfc3339)
-}
