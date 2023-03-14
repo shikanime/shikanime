23d49f284af42c8f137a4b590cef4e139fedd9e6
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-03-14 18:08:10 +0100

    Remove unused Google service account

--- pkgs/terraform-google-shikanime/sa.tf
+++
@@ -1,5 +0,0 @@
-resource "google_service_account" "github_action" {
-  project      = var.google.project
-  account_id   = "${var.name}-github-action"
-  display_name = "Github Actions default service account"
-}
