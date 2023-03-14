96ff709b8ca0f0a746ae55b112b9b30f175e2155
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-03-14 18:54:27 +0100

    Add Gitlab

---
+++ pkgs/terraform-google-shikanime/ssh.tf
@@ -0,0 +1,17 @@
+resource "github_user_ssh_key" "default" {
+  for_each = var.ssh_keys
+  title    = title(each.value.name)
+  key      = each.value.public_key_openssh
+}
+
+resource "time_rotating" "gitlab_ssh_key_expiration" {
+  rotation_months = 3
+}
+
+resource "gitlab_user_sshkey" "default" {
+  for_each   = var.ssh_keys
+  user_id    = data.gitlab_user.current.id
+  title      = title(var.name)
+  key        = each.value.public_key_openssh
+  expires_at = formatdate("YYYY-MM-DDTHH:MM:SSZ", time_rotating.gitlab_ssh_key_expiration.rotation_rfc3339)
+}
--- pkgs/terraform-google-shikanime/variable.tf
+++ pkgs/terraform-google-shikanime/variable.tf
@@ -10,15 +10,26 @@ variable "environment" {
   default     = "studio"
 }
 
+variable "gitlab" {
+  type = object({
+    owner = string
+  })
+  description = "GitLab configuration"
+  default = {
+    owner = "shikalegend"
+  }
+}
+
 variable "github" {
   type = object({
-    organization = string
+    owner = string
     reviewers = map(object({
       username = string
     }))
   })
+  description = "GitHub configuration"
   default = {
-    organization = "shikanime"
+    owner = "shikanime"
     reviewers = {
       shikanime = {
         username = "shikanime"
@@ -32,6 +43,21 @@ variable "ssh_keys" {
     name               = string
     public_key_openssh = string
   }))
+  description = "SSH keys"
+  default = {
+    shikanime = {
+      name               = "shikanime"
+      public_key_openssh = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFpVHzBrvtlzh0eLr6qNttBd5NBZawNJ7PtcAuHJ6pc7 william.phetsinorath@shikanime.studio"
+    }
+    sfeir = {
+      name               = "sfeir"
+      public_key_openssh = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM2+ZmUgNPNepyMTJnjcu6LoGxEJh5ny8Oxe7bLuFx9q phetsinorath.w@sfeir.com"
+    }
+    paprec = {
+      name               = "paprec"
+      public_key_openssh = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDsdYurmBmZpWrDV5EJWZlcCPCU3yTyv1uoe7TzQsYAy phetsinorath.w@sfeir.com"
+    }
+  }
 }
 
 variable "cachix" {
