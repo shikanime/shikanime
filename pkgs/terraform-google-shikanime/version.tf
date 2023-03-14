f952e73d2469d8ba5c97d646ccb11108c44d4d38
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-03-14 18:08:10 +0100

    Remove unused Google service account

--- pkgs/terraform-google-shikanime/version.tf
+++ pkgs/terraform-google-shikanime/version.tf
@@ -1,14 +1,6 @@
 terraform {
   required_version = "~> 1.0"
   required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 4.57"
-    }
-    google-beta = {
-      source  = "hashicorp/google-beta"
-      version = "~> 4.57"
-    }
     github = {
       source  = "integrations/github"
       version = "~> 5.18"
