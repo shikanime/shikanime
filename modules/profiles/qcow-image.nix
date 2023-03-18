adef455abec754cf351f06adfe8fcbb16b3d99d1
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-03-18 12:28:13 +0100

    Fix missing file systems error

--- modules/profiles/qcow-image.nix
+++ modules/profiles/qcow-image.nix
@@ -3,21 +3,9 @@
 with lib;
 
 {
-  # for virtio kernel drivers
   imports = [
     "${modulesPath}/profiles/qemu-guest.nix"
   ];
-
-  fileSystems."/" = {
-    device = "/dev/disk/by-label/nixos";
-    autoResize = true;
-    fsType = "ext4";
-  };
-
-  boot.growPartition = true;
-  boot.kernelParams = [ "console=ttyS0" ];
-  boot.loader.grub.device = "/dev/vda";
-  boot.loader.timeout = mkDefault 0;
 
   system.build.qcowImage = import "${pkgs.path}/nixos/lib/make-disk-image.nix" {
     inherit lib config pkgs;
