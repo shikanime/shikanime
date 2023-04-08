4ec89143be91000683b24c91e7ea62f66f2a6bef
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-04-08 14:06:03 +0200

    Add filesystem

--- modules/profiles/nishir.nix
+++ modules/profiles/nishir.nix
@@ -10,6 +10,26 @@
     };
   };
 
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/nixos";
+    fsType = "ext4";
+    options = [ "noatime" ];
+  };
+
+  boot = {
+    tmpOnTmpfs = true;
+    kernelParams = [
+      "8250.nr_uarts=1"
+      "console=ttyAMA0,115200"
+      "console=tty1"
+      "cma=128M"
+    ];
+    loader.raspberryPi = {
+      enable = true;
+      version = 4;
+    };
+  };
+
   # Enable GPU acceleration
   hardware.raspberry-pi."4".fkms-3d.enable = true;
 
