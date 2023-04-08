4936e3f5d66088ca792c359b33c09efa4b1ca5db
Shikanime Deva <deva.shikanime@protonmail.com>
2023-04-08 13:37:59 +0000

    Refactor profiles in hosts

--- modules/hosts/elkia.nix
+++ modules/hosts/elkia.nix
@@ -7,7 +7,14 @@ with lib;
     "${modulesPath}/profiles/qemu-guest.nix"
     "${modulesPath}/profiles/headless.nix"
     "${modulesPath}/profiles/hardened.nix"
+    "${modulesPath}/virtualisation/qemu-vm.nix"
   ];
+
+  # Configure Home Manager to use NixOS global packages
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+  };
 
   system.build.qcowImage = import "${pkgs.path}/nixos/lib/make-disk-image.nix" {
     inherit lib config pkgs;
--- modules/virtualisation/docker.nix
+++
@@ -1,7 +0,0 @@
-{
-  # Iptables do not work in Docker.
-  networking.firewall.enable = false;
-
-  # Socket activated ssh presents problem in Docker.
-  services.openssh.startWhenNeeded = false;
-}
