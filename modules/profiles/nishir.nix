346e84e8abfeb61500e9f93f279bc0c833ad6923
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-04-08 13:46:56 +0200

    Remove uneeded store optim

--- modules/profiles/nishir.nix
+++ modules/profiles/nishir.nix
@@ -11,18 +11,4 @@
 
   # Enable GPU acceleration
   hardware.raspberry-pi."4".fkms-3d.enable = true;
-
-  nix = {
-    autoOptimiseStore = true;
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 30d";
-    };
-    # Free up to 1GiB whenever there is less than 100MiB left.
-    extraOptions = ''
-      min-free = ${toString (100 * 1024 * 1024)}
-      max-free = ${toString (1024 * 1024 * 1024)}
-    '';
-  };
 }
