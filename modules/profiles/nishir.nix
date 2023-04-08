d25c36d4a5c1f07ebf6f7c80a5888b6edb209a71
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-04-08 13:58:32 +0200

    Add K3s

--- modules/profiles/nishir.nix
+++ modules/profiles/nishir.nix
@@ -9,4 +9,12 @@
 
   # Enable GPU acceleration
   hardware.raspberry-pi."4".fkms-3d.enable = true;
+
+  # This is required so that pod can reach the API server (running on port 6443 by default)
+  networking.firewall.allowedTCPPorts = [ 6443 ];
+
+  services.k3s = {
+    enable = true;
+    role = "server";
+  };
 }
