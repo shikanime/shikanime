318c5be8a5b00bd835a6684c0fddeee19675a6c7
Shikanime Deva <deva.shikanime@protonmail.com>
2023-04-09 12:57:53 +0000

    Refactor cv build config

--- pkgs/curriculum-vitae/default.nix
+++ pkgs/curriculum-vitae/default.nix
@@ -9,5 +9,9 @@ stdenv.mkDerivation {
   name = "curriculum-vitae";
   buildInputs = [ texlive.combined.scheme-full ];
   src = cleanSource ./.;
-  installPhase = "cp *.pdf $out";
+  installPhase = ''
+    mkdir -p $out
+    export pdf=$out/curriculum-vitae-${texlive.combined.scheme-full.name}.pdf
+    cp curriculum-vitae.pdf $pdf
+  '';
 }
