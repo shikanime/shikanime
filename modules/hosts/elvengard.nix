b939fe79921443a3d52aeeacb1d52f5ea47f8e7a
Shikanime Deva <deva.shikanime@protonmail.com>
2023-04-09 15:38:15 +0000

    Fix rpi retro compat

--- modules/hosts/elvengard.nix
+++ modules/hosts/elvengard.nix
@@ -10,6 +10,11 @@
 
   boot.kernelPackages = pkgs.linuxKernel.packages.linux_hardened;
 
+  # https://github.com/k2s-io/k3s/issues/2067
+  boot.kernelParams = [
+    "cgroup_enable=memory"
+  ];
+
   # Resize Hyper-V default disk size
   hyperv.baseImageSize = 64 * 1024;
 
