d9fc968b53b716ef6c110e94b41feb836ee7f28b
Shikanime Deva <deva.shikanime@protonmail.com>
2023-04-09 01:40:44 +0000

    Move inotify tools

--- modules/users/devas.nix
+++ modules/users/devas.nix
@@ -61,9 +61,6 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF7pi5OYqzuMkTymIbJUJteIU3dz+OgduiF5O9cA+B7u"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFChPMDHee+8F8tuchk8nLqdzVj1SOfLFv70NH95K6Yf"
     ];
-    packages = [
-      pkgs.inotify-tools
-    ];
   };
 
   # Configure user home
