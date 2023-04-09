61bf0fd55a35f8ee539a6f0410c7138f6665ac83
Shikanime Deva <deva.shikanime@protonmail.com>
2023-04-09 01:40:16 +0000

    Add openssh keys

---
+++ modules/users/nixos.nix
@@ -0,0 +1,6 @@
+{
+  users.users.nixos.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF7pi5OYqzuMkTymIbJUJteIU3dz+OgduiF5O9cA+B7u"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFChPMDHee+8F8tuchk8nLqdzVj1SOfLFv70NH95K6Yf"
+  ];
+}
