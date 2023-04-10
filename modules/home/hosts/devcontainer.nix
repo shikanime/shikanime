5bad9301bc432e446cb35c4643647a5a296032f4
Shikanime Deva <deva.shikanime@protonmail.com>
2023-04-10 17:53:27 +0000

    Fix missing user from nix config

--- modules/home/hosts/devcontainer.nix
+++ modules/home/hosts/devcontainer.nix
@@ -1,4 +1,6 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
+
+with lib;
 
 {
   home.homeDirectory = "/home/vscode";
@@ -7,4 +9,10 @@
   targets.genericLinux.enable = true;
 
   nix.package = pkgs.nix;
+
+  programs.zsh.initExtra = mkBefore ''
+    # Check if user environment variables are set because containers doesn't set
+    # them by default and Home Manager needs them to work properly
+    export USER=''${USER:-$(whoami)}
+  '';
 }
