e0205e6b72e7de72eb438e1373a1bab886eb6658
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-04-12 15:17:32 +0000

    Update ssh keys sha

--- modules/users/nixos.nix
+++ modules/users/nixos.nix
@@ -2,7 +2,7 @@
   users.users.nixos.openssh.authorizedKeys.keyFiles = [
     (builtins.fetchurl {
       url = "https://github.com/shikanime.keys";
-      sha256 = "sha256:0y83aiibdbh49zw9qrhy8m418556lrpdbpdh477d4qwdial650wh";
+      sha256 = "sha256:0gz0n7rbxlxjn7h6gn6ag1i3d66ziqj39943cka52m827c2q9a5k";
     })
   ];
 }
