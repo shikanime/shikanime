ab5ee4972753d825b2391bc8af934356fafaf23d
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-04-13 17:18:03 +0200

    Add VSC config in devcontainer

--- flake.nix
+++ flake.nix
@@ -157,6 +157,7 @@
             ./modules/home/profiles/base.nix
             ./modules/home/profiles/workstation.nix
             ./modules/home/profiles/xdg.nix
+            ./modules/home/profiles/vcs.nix
           ];
         };
       };
