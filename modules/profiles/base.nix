fb3a30a0f40050f7c60a21fda8727b1191eea512
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-04-25 20:08:11 +0000

    Add system automatic upgrade

--- modules/profiles/base.nix
+++ modules/profiles/base.nix
@@ -9,6 +9,12 @@
     options = "--delete-older-than 30d";
   };
 
+  # Automatically upgrade NixOS
+  system.autoUpgrade = {
+    enable = true;
+    flake = "github:shikanime/shikanime";
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken It‘s perfectly fine and recommended to leave
