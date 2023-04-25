6b1ca0251f2e491ab469901531a451bdc78fd4d4
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-04-25 20:19:31 +0000

    Refactor to flake exposed

--- flake.nix
+++ flake.nix
@@ -25,156 +25,150 @@
     ];
   };
 
-  outputs = { self, nixpkgs, nixos-hardware, home-manager, devenv, ... }@inputs:
-    let supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" ]; in
-    {
-      packages = nixpkgs.lib.genAttrs supportedSystems (system:
-        let pkgs = import nixpkgs { inherit system; }; in {
-          curriculum-vitae = pkgs.callPackage ./pkgs/curriculum-vitae/default.nix { };
-          elkia = self.nixosConfigurations.elkia.config.system.build.qcowImage;
-          elvengard = self.nixosConfigurations.elvengard.config.system.build.hypervImage;
-          nishir = self.nixosConfigurations.nishir.config.system.build.sdImage;
-        }
-      );
+  outputs = { self, nixpkgs, nixos-hardware, home-manager, devenv, ... }@inputs: {
+    packages = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (system:
+      let pkgs = import nixpkgs { inherit system; }; in {
+        curriculum-vitae = pkgs.callPackage ./pkgs/curriculum-vitae/default.nix { };
+        elkia = self.nixosConfigurations.elkia.config.system.build.qcowImage;
+        elvengard = self.nixosConfigurations.elvengard.config.system.build.hypervImage;
+        nishir = self.nixosConfigurations.nishir.config.system.build.sdImage;
+      }
+    );
 
-      devShells = nixpkgs.lib.genAttrs supportedSystems (system:
-        let pkgs = import nixpkgs { inherit system; }; in {
-          default = devenv.lib.mkShell {
-            inherit inputs pkgs;
-            modules = [
-              ./modules/devenv/base.nix
-              ./modules/devenv/latex.nix
-              ./modules/devenv/cloud.nix
-            ];
-          };
-        }
-      );
+    devShells = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (system:
+      let pkgs = import nixpkgs { inherit system; }; in {
+        default = devenv.lib.mkShell {
+          inherit inputs pkgs;
+          modules = [
+            ./modules/devenv/base.nix
+            ./modules/devenv/latex.nix
+            ./modules/devenv/cloud.nix
+          ];
+        };
+      }
+    );
 
-      nixosConfigurations = {
-        elkia = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            ./modules/hosts/elkia.nix
-            ./modules/profiles/base.nix
-            ./modules/profiles/machine.nix
-            ./modules/profiles/workstation.nix
-            ./modules/profiles/syncthing.nix
-            ./modules/profiles/jetbrains.nix
-            ./modules/profiles/vscode.nix
-            ./modules/users/vscode.nix
-            home-manager.nixosModules.home-manager
-          ];
-        };
-        elvengard = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            ./modules/hosts/elvengard.nix
-            ./modules/profiles/base.nix
-            ./modules/profiles/machine.nix
-            ./modules/profiles/workstation.nix
-            ./modules/profiles/syncthing.nix
-            ./modules/profiles/jetbrains.nix
-            ./modules/profiles/vscode.nix
-            ./modules/users/vscode.nix
-            home-manager.nixosModules.home-manager
-          ];
-        };
-        oceando = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            ./modules/hosts/oceando.nix
-            ./modules/profiles/base.nix
-            ./modules/profiles/machine.nix
-            ./modules/profiles/workstation.nix
-            ./modules/profiles/syncthing.nix
-            ./modules/profiles/jetbrains.nix
-            ./modules/profiles/vscode.nix
-            ./modules/users/vscode.nix
-            home-manager.nixosModules.home-manager
-          ];
-        };
-        nishir = nixpkgs.lib.nixosSystem {
-          system = "aarch64-linux";
-          modules = [
-            ./modules/hosts/nishir.nix
-            ./modules/profiles/base.nix
-            ./modules/profiles/machine.nix
-            ./modules/users/nixos.nix
-            nixos-hardware.nixosModules.raspberry-pi-4
-          ];
-        };
+    nixosConfigurations = {
+      elkia = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./modules/hosts/elkia.nix
+          ./modules/profiles/base.nix
+          ./modules/profiles/machine.nix
+          ./modules/profiles/workstation.nix
+          ./modules/profiles/syncthing.nix
+          ./modules/profiles/jetbrains.nix
+          ./modules/profiles/vscode.nix
+          ./modules/users/vscode.nix
+          home-manager.nixosModules.home-manager
+        ];
       };
-
-      homeConfigurations = {
-        "williamphetsinorath@altashar" = home-manager.lib.homeManagerConfiguration {
-          pkgs = import nixpkgs {
-            system = "x86_64-darwin";
-            config.allowUnfree = true;
-          };
-          modules = [
-            ./modules/home/hosts/altashar.nix
-            ./modules/home/users/sfeir.nix
-            ./modules/home/profiles/base.nix
-            ./modules/home/profiles/workstation.nix
-            ./modules/home/profiles/vcs.nix
-            ./modules/home/profiles/cc.nix
-            ./modules/home/profiles/ruby.nix
-            ./modules/home/profiles/beam.nix
-            ./modules/home/profiles/go.nix
-            ./modules/home/profiles/opam.nix
-            ./modules/home/profiles/rust.nix
-            ./modules/home/profiles/python.nix
-            ./modules/home/profiles/web.nix
-            ./modules/home/profiles/latex.nix
-            ./modules/home/profiles/sql.nix
-            ./modules/home/profiles/cloud.nix
-            ./modules/home/profiles/java.nix
-            ./modules/home/profiles/experimental/github-copilot-cli.nix
-          ];
-        };
-        "devas@ishtar" = home-manager.lib.homeManagerConfiguration {
-          pkgs = import nixpkgs {
-            system = "x86_64-linux";
-            config.allowUnfree = true;
-          };
-          modules = [
-            ./modules/home/hosts/ishtar.nix
-            ./modules/home/users/sfeir.nix
-            ./modules/home/profiles/base.nix
-            ./modules/home/profiles/workstation.nix
-            ./modules/home/profiles/xdg.nix
-            ./modules/home/profiles/vcs.nix
-            ./modules/home/profiles/cc.nix
-            ./modules/home/profiles/ruby.nix
-            ./modules/home/profiles/beam.nix
-            ./modules/home/profiles/go.nix
-            ./modules/home/profiles/opam.nix
-            ./modules/home/profiles/rust.nix
-            ./modules/home/profiles/python.nix
-            ./modules/home/profiles/web.nix
-            ./modules/home/profiles/latex.nix
-            ./modules/home/profiles/sql.nix
-            ./modules/home/profiles/cloud.nix
-            ./modules/home/profiles/java.nix
-          ];
-        };
-        vscode = home-manager.lib.homeManagerConfiguration {
-          pkgs = import nixpkgs {
-            system = "x86_64-linux";
-            config.allowUnfree = true;
-          };
-          modules = [
-            ./modules/home/hosts/devcontainer.nix
-            ./modules/home/users/totalenergies.nix
-            ./modules/home/users/sfeir.nix
-            ./modules/home/users/paprec.nix
-            ./modules/home/users/galec.nix
-            ./modules/home/users/birdz.nix
-            ./modules/home/profiles/base.nix
-            ./modules/home/profiles/workstation.nix
-            ./modules/home/profiles/xdg.nix
-          ];
-        };
+      elvengard = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./modules/hosts/elvengard.nix
+          ./modules/profiles/base.nix
+          ./modules/profiles/machine.nix
+          ./modules/profiles/workstation.nix
+          ./modules/profiles/syncthing.nix
+          ./modules/profiles/jetbrains.nix
+          ./modules/profiles/vscode.nix
+          ./modules/users/vscode.nix
+          home-manager.nixosModules.home-manager
+        ];
+      };
+      oceando = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./modules/hosts/oceando.nix
+          ./modules/profiles/base.nix
+          ./modules/profiles/machine.nix
+          ./modules/profiles/workstation.nix
+          ./modules/profiles/syncthing.nix
+          ./modules/profiles/jetbrains.nix
+          ./modules/profiles/vscode.nix
+          ./modules/users/vscode.nix
+          home-manager.nixosModules.home-manager
+        ];
+      };
+      nishir = nixpkgs.lib.nixosSystem {
+        system = "aarch64-linux";
+        modules = [
+          ./modules/hosts/nishir.nix
+          ./modules/profiles/base.nix
+          ./modules/profiles/machine.nix
+          ./modules/users/nixos.nix
+          nixos-hardware.nixosModules.raspberry-pi-4
+        ];
       };
     };
+
+    homeConfigurations = {
+      "williamphetsinorath@altashar" = home-manager.lib.homeManagerConfiguration {
+        pkgs = import nixpkgs {
+          system = "x86_64-darwin";
+          config.allowUnfree = true;
+        };
+        modules = [
+          ./modules/home/hosts/altashar.nix
+          ./modules/home/users/sfeir.nix
+          ./modules/home/profiles/base.nix
+          ./modules/home/profiles/workstation.nix
+          ./modules/home/profiles/vcs.nix
+          ./modules/home/profiles/cc.nix
+          ./modules/home/profiles/ruby.nix
+          ./modules/home/profiles/beam.nix
+          ./modules/home/profiles/go.nix
+          ./modules/home/profiles/opam.nix
+          ./modules/home/profiles/rust.nix
+          ./modules/home/profiles/python.nix
+          ./modules/home/profiles/web.nix
+          ./modules/home/profiles/latex.nix
+          ./modules/home/profiles/sql.nix
+          ./modules/home/profiles/cloud.nix
+          ./modules/home/profiles/java.nix
+          ./modules/home/profiles/experimental/github-copilot-cli.nix
+        ];
+      };
+      "devas@ishtar" = home-manager.lib.homeManagerConfiguration {
+        pkgs = import nixpkgs {
+          system = "x86_64-linux";
+          config.allowUnfree = true;
+        };
+        modules = [
+          ./modules/home/hosts/ishtar.nix
+          ./modules/home/users/sfeir.nix
+          ./modules/home/profiles/base.nix
+          ./modules/home/profiles/workstation.nix
+          ./modules/home/profiles/xdg.nix
+          ./modules/home/profiles/vcs.nix
+          ./modules/home/profiles/cc.nix
+          ./modules/home/profiles/ruby.nix
+          ./modules/home/profiles/beam.nix
+          ./modules/home/profiles/go.nix
+          ./modules/home/profiles/opam.nix
+          ./modules/home/profiles/rust.nix
+          ./modules/home/profiles/python.nix
+          ./modules/home/profiles/web.nix
+          ./modules/home/profiles/latex.nix
+          ./modules/home/profiles/sql.nix
+          ./modules/home/profiles/cloud.nix
+          ./modules/home/profiles/java.nix
+        ];
+      };
+      vscode = home-manager.lib.homeManagerConfiguration {
+        pkgs = import nixpkgs {
+          system = "x86_64-linux";
+          config.allowUnfree = true;
+        };
+        modules = [
+          ./modules/home/hosts/devcontainer.nix
+          ./modules/home/users/sfeir.nix
+          ./modules/home/profiles/base.nix
+          ./modules/home/profiles/workstation.nix
+          ./modules/home/profiles/xdg.nix
+        ];
+      };
+    };
+  };
 }
