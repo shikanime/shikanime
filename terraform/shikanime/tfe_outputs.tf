c79829c6123fe299fe6a27d6b32947d00836af04
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-06-09 12:59:38 +0000

    Fix workspace outputs

--- terraform/shikanime/tfe_outputs.tf
+++ terraform/shikanime/tfe_outputs.tf
@@ -1,18 +1,5 @@
 data "tfe_outputs" "default" {
-  for_each = {
-    shikanime = {
-      workspace_name = "shikanime"
-    }
-    studio = {
-      workspace_name = "shikanime-studio"
-    }
-    studio-labs = {
-      workspace_name = "shikanime-studio-labs"
-    }
-    totalenergies = {
-      workspace_name = "shikanime-totalenergies"
-    }
-  }
+  for_each     = tfe_workspace.default
   organization = tfe_organization.default.name
-  workspace    = each.value.workspace_name
+  workspace    = each.value.name
 }
