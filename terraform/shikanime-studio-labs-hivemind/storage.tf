286699281a8f236761d6ae8ef6909b163645520f
Shikanime Deva <william.phetsinorath@shikanime.studio>
2023-06-09 20:02:50 +0200

    Rename storage bucket

--- terraform/shikanime-studio-labs-hivemind/storage.tf
+++ terraform/shikanime-studio-labs-hivemind/storage.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "deep_lake" {
   project  = var.project_id
-  name     = "${var.project_id}-hivemind-deep-lake-dataset"
+  name     = "${var.project_id}-hivemind-deep-lake-datasets"
   location = "EU"
 }
