e78ea938bb324e855719650bf752d3b1648c63a5
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-06-09 13:28:57 +0000

    Reword wip display name

--- terraform/shikanime-studio-labs/iam.tf
+++ terraform/shikanime-studio-labs/iam.tf
@@ -1,7 +1,7 @@
 resource "google_iam_workload_identity_pool" "tfc" {
   project                   = module.google_project.project_id
   workload_identity_pool_id = "tfc-pool"
-  display_name              = "TFC Pool"
+  display_name              = "TFC"
   description               = "Identity pool for Terraform Cloud Dynamic Credentials integration"
 }
 
@@ -9,7 +9,7 @@ resource "google_iam_workload_identity_pool_provider" "tfc" {
   project                            = module.google_project.project_id
   workload_identity_pool_id          = google_iam_workload_identity_pool.tfc.workload_identity_pool_id
   workload_identity_pool_provider_id = "tfc-provider"
-  display_name                       = "TFC Pool Provider"
+  display_name                       = "TFC"
   description                        = "OIDC identity pool provider for Terraform Cloud Dynamic Credentials integration"
   # Use condition to make sure only token generated for a specific TFC Org can be used across org workspaces
   attribute_condition = "attribute.terraform_organization_id == \"${var.tfc_organization_id}\""
