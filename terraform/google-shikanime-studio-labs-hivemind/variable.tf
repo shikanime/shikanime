497f2729ea9841434ae36d749e60a1d6735b850a
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-06-23 12:48:27 +0000

    Add automatic run trigger

--- terraform/google-shikanime-studio-labs-hivemind/variable.tf
+++
@@ -1,5 +0,0 @@
-variable "project_id" {
-  description = "The project id"
-  type        = string
-  default     = "shikanime-studio-labs"
-}
---
+++ terraform/tfe-shikanime/run_trigger.tf
@@ -0,0 +1,12 @@
+resource "tfe_run_trigger" "tfc" {
+  for_each = {
+    google-shikanime-studio = {
+      workspace_id = tfe_workspace.default["tfe-shikanime"].id
+    }
+    google-shikanime-studio-labs = {
+      workspace_id = tfe_workspace.default["tfe-shikanime"].id
+    }
+  }
+  workspace_id  = each.value.workspace_id
+  sourceable_id = tfe_workspace.default[each.key].id
+}
