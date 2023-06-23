aa04cddfe29526f23043b318c7d0ababd1a644a4
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-06-23 11:39:00 +0000

    Fix varset segmentation

--- terraform/tfe-shikanime/tfe_variable.tf
+++ terraform/tfe-shikanime/tfe_variable.tf
@@ -1,16 +1,17 @@
 resource "tfe_variable_set" "tfc" {
   for_each = {
-    tfe-shikanime-studio = {}
+    tfe-shikanime = {}
   }
-  name         = "TFC (${each.value.name})"
-  description  = "TFC variables for ${each.value.name}"
+  name         = "TFC (${each.key})"
+  description  = "TFC variables for ${each.key}"
   organization = tfe_organization.default.name
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "tfe_variable" "tfc_organization_id" {
-  for_each = {
-    tfe-shikanime-studio = {}
-  }
+  for_each        = tfe_variable_set.tfc
   key             = "tfc_organization_id"
   value           = data.tfe_organization.default.external_id
   category        = "terraform"
@@ -19,7 +20,7 @@ resource "tfe_variable" "tfc_organization_id" {
 }
 
 resource "tfe_variable" "tfc_workspace_id" {
-  for_each        = tfe_workspace.default
+  for_each        = tfe_variable_set.tfc
   key             = "tfc_workspace_id"
   value           = each.value.id
   category        = "terraform"
@@ -27,23 +28,17 @@ resource "tfe_variable" "tfc_workspace_id" {
   variable_set_id = tfe_variable_set.tfc[each.key].id
 }
 
-resource "tfe_variable_set" "cloudflare_provider" {
-  for_each = {
-    cloudflare-shikanime-studio = {}
-  }
-  name         = "Cloudflare Provider (${each.value.name})"
-  description  = "Cloudflare Provider variables for ${each.value.name}"
-  organization = tfe_organization.default.name
-}
-
 resource "tfe_variable_set" "google_provider" {
   for_each = {
-    google-shikanime-studio      = {}
-    google-shikanime-studio-labs = {}
+    shikanime-studio      = {}
+    shikanime-studio-labs = {}
   }
   name         = "Google Provider (${each.key})"
   description  = "Google Provider variables for ${each.key}"
   organization = tfe_organization.default.name
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "tfe_variable" "tfc_google_provider_auth" {
@@ -56,37 +51,90 @@ resource "tfe_variable" "tfc_google_provider_auth" {
 }
 
 resource "tfe_variable" "tfc_google_project_number" {
-  for_each        = tfe_variable_set.google_provider
+  for_each = {
+    shikanime-studio = {
+      value = lookup(data.tfe_outputs.default["google-shikanime-studio"].values, "project_number", null)
+    }
+    shikanime-studio-labs = {
+      value = lookup(data.tfe_outputs.default["google-shikanime-studio-labs"].values, "project_number", null)
+    }
+  }
   key             = "TFC_GCP_PROJECT_NUMBER"
-  value           = lookup(data.tfe_outputs.default[each.key].values, "project_number", null)
+  value           = each.value.value
   category        = "env"
   description     = "Project number"
   variable_set_id = tfe_variable_set.google_provider[each.key].id
 }
 
 resource "tfe_variable" "tfc_google_workload_pool_id" {
-  for_each        = tfe_variable_set.google_provider
+  for_each = {
+    shikanime-studio = {
+      value = lookup(data.tfe_outputs.default["google-shikanime-studio"].values, "workload_identity_pool_id", null)
+    }
+    shikanime-studio-labs = {
+      value = lookup(data.tfe_outputs.default["google-shikanime-studio-labs"].values, "workload_identity_pool_id", null)
+    }
+  }
   key             = "TFC_GCP_WORKLOAD_POOL_ID"
-  value           = lookup(data.tfe_outputs.default[each.key].values, "workload_identity_pool_id", null)
+  value           = each.value.value
   category        = "env"
   description     = "Workload pool id"
   variable_set_id = tfe_variable_set.google_provider[each.key].id
 }
 
 resource "tfe_variable" "tfc_workload_provider_id" {
-  for_each        = tfe_variable_set.google_provider
+  for_each = {
+    shikanime-studio = {
+      value = lookup(data.tfe_outputs.default["google-shikanime-studio"].values, "workload_identity_pool_provider_id", null)
+    }
+    shikanime-studio-labs = {
+      value = lookup(data.tfe_outputs.default["google-shikanime-studio-labs"].values, "workload_identity_pool_provider_id", null)
+    }
+  }
   key             = "TFC_GCP_WORKLOAD_PROVIDER_ID"
-  value           = lookup(data.tfe_outputs.default[each.key].values, "workload_identity_pool_provider_id", null)
+  value           = each.value.value
   category        = "env"
   description     = "Workload provider id"
   variable_set_id = tfe_variable_set.google_provider[each.key].id
 }
 
 resource "tfe_variable" "tfc_run_service_account_email" {
-  for_each        = tfe_variable_set.google_provider
+  for_each = {
+    shikanime-studio = {
+      value = lookup(data.tfe_outputs.default["google-shikanime-studio"].values, "run_service_account_email", null)
+    }
+    shikanime-studio-labs = {
+      value = lookup(data.tfe_outputs.default["google-shikanime-studio-labs"].values, "run_service_account_email", null)
+    }
+  }
   key             = "TFC_GCP_RUN_SERVICE_ACCOUNT_EMAIL"
-  value           = lookup(data.tfe_outputs.default[each.key].values, "run_service_account_email", null)
+  value           = each.value.value
   category        = "env"
   description     = "Run service account email"
   variable_set_id = tfe_variable_set.google_provider[each.key].id
+}
+
+resource "tfe_variable_set" "github_provider" {
+  for_each = {
+    shikanime     = {}
+    totalenergies = {}
+  }
+  name         = "GitHub Provider (${each.key})"
+  description  = "GitHub Provider variables for ${each.key}"
+  organization = tfe_organization.default.name
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "tfe_variable_set" "cloudflare_provider" {
+  for_each = {
+    shikanime-studio = {}
+  }
+  name         = "Cloudflare Provider (${each.key})"
+  description  = "Cloudflare Provider variables for ${each.key}"
+  organization = tfe_organization.default.name
+  lifecycle {
+    prevent_destroy = true
+  }
 }
