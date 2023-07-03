3f013be0b6bc88b86be17afc08ee5f79561c2652
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-07-03 22:39:52 +0000

    Refactor Terraform Vercel project management

--- terraform/vercel-shikanime-studio/project.tf
+++ terraform/vercel-shikanime-studio/project.tf
@@ -1,15 +1,33 @@
-resource "vercel_project" "links" {
-  name                       = "links"
+resource "vercel_project" "default" {
+  for_each = {
+    links = {
+      name = "links"
+      root_directory = "pkgs/links"
+    }
+    infinity_horizons = {
+      name = "infinity-horizons"
+      root_directory = "pkgs/infinity-horizons"
+    }
+  }
+  name                       = each.value.name
   framework                  = "nextjs"
   serverless_function_region = "fra1"
-  root_directory             = "pkgs/links"
+  root_directory             = each.value.root_directory
   git_repository = {
     type = "github"
     repo = "shikanime/shikanime"
   }
 }
 
-resource "vercel_project_domain" "links" {
-  project_id = vercel_project.links.id
-  domain     = "links.shikanime.studio"
+resource "vercel_project_domain" "default" {
+  for_each = {
+    links = {
+      domain = "links.shikanime.studio"
+    }
+    infinity_horizons = {
+      domain = "infinityhorizons.shikanime.studio"
+    }
+  }
+  project_id = vercel_project.default[each.key].id
+  domain     = each.value.domain
 }
