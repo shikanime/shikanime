9bf7da658c229acd02eb8e3a0b01035a2a52f7c6
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-07-03 22:51:55 +0000

    Fix links margin

--- pkgs/links/app/page.tsx
+++ pkgs/links/app/page.tsx
@@ -3,30 +3,28 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="hero min-h-screen">
-      <div className="hero-content flex-col space-y-4">
-        <div className="mt-16">
-          <label className="swap">
-            <input type="checkbox" />
-            <div className="swap-on">
-              <Image
-                src="/avatar-happy.webp"
-                alt="avatar-happy#user"
-                width={150}
-                height={150}
-                className="rounded-full border-2 border-white"
-              />
-            </div>
-            <div className="swap-off">
-              <Image
-                src="/avatar.webp"
-                alt="avatar#user"
-                width={150}
-                height={150}
-                className="rounded-full border-2 border-white"
-              />
-            </div>
-          </label>
-        </div>
+      <div className="hero-content flex-col space-y-4 my-4">
+        <label className="swap">
+          <input type="checkbox" />
+          <div className="swap-on">
+            <Image
+              src="/avatar-happy.webp"
+              alt="avatar-happy#user"
+              width={150}
+              height={150}
+              className="rounded-full border-2 border-white"
+            />
+          </div>
+          <div className="swap-off">
+            <Image
+              src="/avatar.webp"
+              alt="avatar#user"
+              width={150}
+              height={150}
+              className="rounded-full border-2 border-white"
+            />
+          </div>
+        </label>
 
         <div className="text-center">
           <h2 className="text-2xl font-mono font-bold text-white">
