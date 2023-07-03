8ea2e7ac8f62a590d4156d70f6a8565ac0d2b6fd
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-07-03 22:46:01 +0000

    Change IH metadata

--- pkgs/infinity-horizons/app/layout.tsx
+++ pkgs/infinity-horizons/app/layout.tsx
@@ -5,8 +5,10 @@ import { Analytics } from "@vercel/analytics/react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Shikanime",
-  description: "Shikanime",
+  title: "Infinity Horizons",
+  description:
+    "Where ideas collide and imagination soars, we are proud " +
+    "dreamers on the mission to make them a reality",
 };
 
 export default function RootLayout({
