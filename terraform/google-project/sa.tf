76255035d5048b2399c184b2f934cac71f095b71
Shikanime Deva <william.phetsinorath@shikanime.studio>
2023-07-10 15:28:47 +0200

    Rename Terraform Service Account

--- terraform/google-project/sa.tf
+++ terraform/google-project/sa.tf
@@ -3,13 +3,14 @@ module "tfc_service_account" {
   version = "~> 3.0"
 
   project_id   = module.google_project.project_id
-  names        = ["terraform-cloud"]
-  display_name = "Terraform Cloud service account"
+  names        = ["tfc-service-account"]
+  display_name = "Terraform Cloud Service account"
 
   project_roles = [
     "${module.google_project.project_id}=>roles/browser",
     "${module.google_project.project_id}=>roles/iam.securityAdmin",
     "${module.google_project.project_id}=>roles/iam.workloadIdentityPoolAdmin",
-    "${module.google_project.project_id}=>roles/serviceusage.serviceUsageAdmin"
+    "${module.google_project.project_id}=>roles/serviceusage.serviceUsageAdmin",
+    "${module.google_project.project_id}=>roles/iam.serviceAccountAdmin",
   ]
 }
