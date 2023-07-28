6377888592e83a9800e1f7611c26448b7073edd3
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-07-28 09:59:02 +0000

    Ditch out self routing DNS

--- modules/home/identities/sfeir.nix
+++ modules/home/identities/sfeir.nix
@@ -2,25 +2,14 @@
 
 {
   programs.ssh.matchBlocks = {
-    "gitlab.sfeir.x.shikanime.studio" = {
-      identityFile = "${config.home.homeDirectory}/.ssh/sfeir_ed25519";
-    };
-    "github.sfeir.x.shikanime.studio" = {
-      identityFile = "${config.home.homeDirectory}/.ssh/sfeir_ed25519";
+    "gitlab.com" = {
+      identityFile = [ "${config.home.homeDirectory}/.ssh/sfeir_ed25519" ];
     };
   };
 
   programs.git.includes = [
     {
-      condition = "hasconfig:remote.*.url:git@gitlab.sfeir.x.shikanime.studio:**";
-      contents.user = {
-        name = "William Phetsinorath";
-        email = "phetsinorath.w@sfeir.com";
-        signingKey = "2EC6BC5847E93460";
-      };
-    }
-    {
-      condition = "hasconfig:remote.*.url:git@github.sfeir.x.shikanime.studio:**";
+      condition = "hasconfig:remote.*.url:git@gitlab.com:*/Sfeir/**";
       contents.user = {
         name = "William Phetsinorath";
         email = "phetsinorath.w@sfeir.com";
