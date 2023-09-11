e640826b8b3583c5e6d70375bd4f5e8abe619ad9
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-09-11 16:18:50 +0000

    Add NodeJS for extensions

--- modules/home/hosts/devcontainer.nix
+++ modules/home/hosts/devcontainer.nix
@@ -6,6 +6,7 @@ with lib;
   imports = [
     ../identities/sfeir.nix
     ../profiles/base.nix
+    ../profiles/javascript.nix
     ../profiles/workstation.nix
     ../profiles/xdg.nix
     ../profiles/vcs.nix
