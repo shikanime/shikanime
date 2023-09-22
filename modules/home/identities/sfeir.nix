46e3d099c87ac824d59f192ce7b5edbac62b9bac
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-09-22 08:35:42 +0000

    Add user accounts

--- modules/home/identities/sfeir.nix
+++ modules/home/identities/sfeir.nix
@@ -16,5 +16,13 @@
         signingKey = "2EC6BC5847E93460";
       };
     }
+    {
+      condition = "hasconfig:remote.*.url:git@gitlab.com:phetsinorath.w/**";
+      contents.user = {
+        name = "William Phetsinorath";
+        email = "phetsinorath.w@sfeir.com";
+        signingKey = "2EC6BC5847E93460";
+      };
+    }
   ];
 }
