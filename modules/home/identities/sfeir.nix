d7078a04320aac68b7940aeaf30cae4c20643387
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-12-01 10:15:47 +0000

    Add Git SSH redirection

--- modules/home/identities/sfeir.nix
+++ modules/home/identities/sfeir.nix
@@ -1,19 +1,33 @@
+{ config, ... }:
+
 {
+  programs.ssh.matchBlocks = {
+    "sfeir.internal" = {
+      identityFile = [ "${config.home.homeDirectory}/.ssh/sfeir_ed25519" ];
+    };
+  };
+
   programs.git.includes = [
     {
       condition = "hasconfig:remote.*.url:git@gitlab.com:Sfeir/**";
-      contents.user = {
-        name = "William Phetsinorath";
-        email = "phetsinorath.w@sfeir.com";
-        signingKey = "2EC6BC5847E93460";
+      contents = {
+        user = {
+          name = "William Phetsinorath";
+          email = "phetsinorath.w@sfeir.com";
+          signingKey = "2EC6BC5847E93460";
+        };
+        url."git@gitlab.com:**".insteadOf = "git@sfeir.internal:**";
       };
     }
     {
       condition = "hasconfig:remote.*.url:git@gitlab.com:phetsinorath.w/**";
-      contents.user = {
-        name = "William Phetsinorath";
-        email = "phetsinorath.w@sfeir.com";
-        signingKey = "2EC6BC5847E93460";
+      contents = {
+        user = {
+          name = "William Phetsinorath";
+          email = "phetsinorath.w@sfeir.com";
+          signingKey = "2EC6BC5847E93460";
+        };
+        url."git@gitlab.com:**".insteadOf = "git@sfeir.internal:**";
       };
     }
   ];
