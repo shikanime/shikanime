62c293900a8600c06b34bdc1b696c85a5ca954e9
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-12-02 19:20:59 +0000

    Add Git SSH redirection

--- modules/home/identities/sfeir.nix
+++ modules/home/identities/sfeir.nix
@@ -1,20 +1,33 @@
+{ config, ... }:
+
 {
-  programs.git.includes = [
-    {
-      condition = "hasconfig:remote.*.url:git@gitlab.com:Sfeir/**";
-      contents.user = {
-        name = "William Phetsinorath";
-        email = "phetsinorath.w@sfeir.com";
-        signingKey = "2EC6BC5847E93460";
-      };
-    }
-    {
-      condition = "hasconfig:remote.*.url:git@gitlab.com:phetsinorath.w/**";
-      contents.user = {
-        name = "William Phetsinorath";
-        email = "phetsinorath.w@sfeir.com";
-        signingKey = "2EC6BC5847E93460";
-      };
-    }
-  ];
+  programs.ssh.matchBlocks = {
+    "github.sfeir.internal" = {
+      identityFile = [ "${config.home.homeDirectory}/.ssh/sfeir_ed25519" ];
+    };
+  };
+
+  programs.git.includes =
+    let
+
+      name = "William Phetsinorath";
+      email = "phetsinorath.w@sfeir.com";
+      signingKey = "2EC6BC5847E93460";
+    in
+    [
+      {
+        condition = "hasconfig:remote.*.url:git@gitlab.com:Sfeir/**";
+        contents = {
+          user = { inherit name email signingKey; };
+          url."git@gitlab.com:**".insteadOf = "git@github.sfeir.internal:**";
+        };
+      }
+      {
+        condition = "hasconfig:remote.*.url:git@gitlab.com:phetsinorath.w/**";
+        contents = {
+          user = { inherit name email signingKey; };
+          url."git@gitlab.com:**".insteadOf = "git@github.sfeir.internal:**";
+        };
+      }
+    ];
 }
