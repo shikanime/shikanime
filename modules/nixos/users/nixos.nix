dd75a58e64fca3968daa866f527ce19bfd27b696
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-12-10 00:48:45 +0100

    Refactor NixOS configs

--- modules/nixos/users/nixos.nix
+++ modules/nixos/users/nixos.nix
@@ -1,9 +1,15 @@
 {
-  # Add well-known ssh keys
-  users.users.nixos.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPW5kSIFStkESjXOavgJg75Wfxnsml7+ZT0grMnYNzwC"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAB8e13bjswnhfuYYpztBESPf/gkbkdGE46kC++tNOCX"
-  ];
+  # Create user accounts
+  users.users.nixos = {
+    isNormalUser = true;
+    home = "/home/nixos";
+    extraGroups = [ "wheel" ];
+    initialHashedPassword = "";
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICGJ1GT3xsOpvFmXWOjNkCBjeRNn7tWiFV4bTXoYe3+V"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAB8e13bjswnhfuYYpztBESPf/gkbkdGE46kC++tNOCX"
+    ];
+  };
 
   # Configure user home
   home-manager.users.nixos.imports = [
