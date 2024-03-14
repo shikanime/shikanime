92aeb37d095897ec9696b795e5e1b10ecb5180db
William Phetsinorath <william.phetsinorath@shikanime.studio>
2024-03-14 23:49:06 +0100

    Remove problematic runtime global programs

--- modules/home/profiles/native.nix
+++ modules/home/profiles/native.nix
@@ -1,18 +1,5 @@
-{ pkgs, ... }:
-
 {
   programs.zsh.oh-my-zsh.plugins = [
     "rust"
-  ];
-
-  home.packages = [
-    pkgs.rustup
-    pkgs.cmake
-    pkgs.meson
-    pkgs.ninja
-    pkgs.ccache
-    pkgs.gdb
-    pkgs.lldb
-    pkgs.clang-tools
   ];
 }
--- modules/home/profiles/python.nix
+++ modules/home/profiles/python.nix
@@ -1,15 +1,8 @@
-{ pkgs, ... }:
-
 {
   programs.neovim.withPython3 = true;
 
   programs.zsh.oh-my-zsh.plugins = [
     "python"
     "poetry"
-  ];
-
-  home.packages = [
-    pkgs.python3Full
-    pkgs.pipx
   ];
 }
