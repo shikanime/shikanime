e7a004391a334b99c7068029f169c0e00ad77a1f
William Phetsinorath <william.phetsinorath@shikanime.studio>
2024-04-19 03:04:50 +0200

    Update Nishir to support K3s with Longhorn

---
+++ modules/nixos/users/shika.nix
@@ -0,0 +1,27 @@
+{
+  # Create user accounts
+  users.users.shika = {
+    isNormalUser = true;
+    home = "/home/shika";
+    extraGroups = [ "wheel" ];
+    initialHashedPassword = "$y$j9T$HB1msXB0DEq00J48zRpB20$/3rhVrTzGrv1j/cPvZ0clOM2gEe1TeylUG39wgD0C42";
+  };
+
+  # Configure user home
+  home-manager.users.shika = {
+    imports = [
+      ../../home/profiles/base.nix
+    ];
+
+    programs.gpg.enable = true;
+
+    services.gpg-agent = {
+      enable = true;
+      enableSshSupport = true;
+      enableExtraSocket = true;
+    };
+
+    home.homeDirectory = "/home/shika";
+    home.username = "shika";
+  };
+}
