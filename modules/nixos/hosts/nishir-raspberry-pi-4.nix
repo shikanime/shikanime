06624f47a465cfdc24597ef60817ab1051bd31c8
William Phetsinorath <william.phetsinorath@shikanime.studio>
2024-04-23 20:59:19 +0200

    Re-integrate NixOS configs

--- modules/nixos/hosts/nishir-raspberry-pi-4.nix
+++ modules/nixos/hosts/nishir-raspberry-pi-4.nix
@@ -2,23 +2,8 @@
 
 {
   imports = [
-    "${modulesPath}/profiles/headless.nix"
     "${modulesPath}/installer/sd-card/sd-image-aarch64.nix"
-    ../profiles/base.nix
-    ../profiles/machine.nix
-    ../users/shika.nix
-  ];
-
-  boot.kernelPackages = pkgs.linuxKernel.packages.linux_rpi4;
-
-  boot.kernelParams = [
-    "8250.nr_uarts=1"
-    "console=ttyAMA0,115200"
-    "console=tty1"
-    "cma=128M"
-    "cgroup_enable=cpuset"
-    "cgroup_enable=memory"
-    "cgroup_memory=1"
+    ./nishir-base.nix
   ];
 
   hardware.raspberry-pi."4" = {
@@ -36,15 +21,6 @@
 
   # Enable Wake-on-LAN on the ethernet port
   networking.interfaces.eth0.wakeOnLan.enable = true;
-
-  # This is required so that pod can reach the API server (running on port 6443 by default)
-  networking.firewall.allowedTCPPorts = [ 6443 ];
-
-  # Enable Kubernetes
-  services.k3s = {
-    enable = true;
-    role = "server";
-  };
 
   # Enable disk auto-mounting
   systemd.mounts = [
@@ -71,15 +47,4 @@
       wantedBy = [ "multi-user.target" ];
     }
   ];
-
-  # Longhorn requires open-iscsi
-  services.openiscsi = {
-    enable = true;
-    name = "iqn.2024-03.studio.shikanime:nishir";
-  };
-
-  # Enable cross platform build
-  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
-
-  networking.hostName = "nishir";
 }
