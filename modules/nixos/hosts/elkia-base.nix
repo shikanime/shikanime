ff597281e3a8d2d8dd995044508fb74cf96b5be5
William Phetsinorath <william.phetsinorath@shikanime.studio>
2024-04-23 20:43:22 +0200

    Re-integrate NixOS configs

--- modules/nixos/hosts/elkia-base.nix
+++ modules/nixos/hosts/elkia-base.nix
@@ -1,14 +1,13 @@
-{ config, pkgs, lib, modulesPath, ... }:
+{ pkgs, lib, modulesPath, ... }:
+
+with lib;
 
 {
   imports = [
     "${modulesPath}/profiles/qemu-guest.nix"
     "${modulesPath}/profiles/headless.nix"
-    "${modulesPath}/profiles/hardened.nix"
-    "${modulesPath}/virtualisation/qemu-vm.nix"
     ../profiles/base.nix
     ../profiles/machine.nix
-    ../profiles/editor.nix
     ../profiles/workstation.nix
     ../profiles/syncthing.nix
     ../profiles/jetbrains.nix
@@ -16,15 +15,7 @@
     ../users/vscode.nix
   ];
 
-  boot.kernelPackages = pkgs.linuxKernel.packages.linux_hardened;
-
   boot.kernelParams = [ "cgroup_enable=memory" ];
-
-  system.build.qcowImage = import "${pkgs.path}/nixos/lib/make-disk-image.nix" {
-    inherit lib config pkgs;
-    diskSize = 64 * 1024;
-    format = "qcow2";
-  };
 
   networking.hostName = "elkia";
 }
