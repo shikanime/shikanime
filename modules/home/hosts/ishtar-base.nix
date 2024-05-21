6371a48a8f4b0e4cd2bef1311e4f3318357e33fb
William Phetsinorath <william.phetsinorath@shikanime.studio>
2024-05-21 10:41:58 +0200

    Move git-credential-manager to scoop

--- modules/home/hosts/ishtar-base.nix
+++ modules/home/hosts/ishtar-base.nix
@@ -32,7 +32,7 @@
   targets.genericLinux.enable = true;
 
   programs.git.extraConfig.credential.helper =
-    "/mnt/c/Program\\ Files/Git/mingw64/bin/git-credential-manager.exe";
+    "/mnt/c/User/willi/scoop/shims/git-credential-manager.exe";
 
   # CUDA support
   home.sessionVariables.LD_LIBRARY_PATH = "/usr/lib/wsl/lib";
