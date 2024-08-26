0869329617119edda3b4c58b520cba8143a2c1cf
William Phetsinorath <william.phetsinorath@shikanime.studio>
2024-08-26 10:13:50 +0200

    Fix broken SonarLint

--- modules/home/hosts/baltashar-base.nix
+++ modules/home/hosts/baltashar-base.nix
@@ -10,6 +10,10 @@ let
     if command -v brew >/dev/null; then
       eval "$(brew shellenv)"
     fi
+
+    if [ -d "/opt/homebrew/opt/openjdk/bin" ]; then
+      export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"
+    fi
   '';
 in
 {
