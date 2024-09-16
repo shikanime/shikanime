845ee80c576e98e5c216cff7e5ff0d91c879456c
William Phetsinorath <william.phetsinorath@shikanime.studio>
2024-09-16 21:43:53 +0200

    Add devcontainer toolings

--- modules/home/hosts/devcontainer-vscode.nix
+++ modules/home/hosts/devcontainer-vscode.nix
@@ -7,4 +7,25 @@
     homeDirectory = "/home/vscode";
     username = "vscode";
   };
+
+  home.packages = [
+    pkgs.texlive.combined.scheme-full
+    pkgs.nodejs_22
+    pkgs.terraform
+    (pkgs.google-cloud-sdk.withExtraComponents (
+      with pkgs.google-cloud-sdk.components; [
+        alpha
+        beta
+        bq
+        gsutil
+        gke-gcloud-auth-plugin
+        gcloud-crc32c
+        kubectl
+        kustomize
+        skaffold
+      ]
+    ))
+  ];
+
+  programs.k9s.enable = true;
 }
