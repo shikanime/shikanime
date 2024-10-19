e1a039f262a6428e284756e2e6417f74ec373902
William Phetsinorath <william.phetsinorath@shikanime.studio>
2024-10-19 20:17:53 +0000

    None on "Remove legacy artifacts"
    [ghstack-poisoned]

--- modules/nixos/profiles/k3s.nix
+++ modules/nixos/profiles/k3s.nix
@@ -4,20 +4,6 @@
     role = "server";
   };
 
-  # Longhorn requires open-iscsi
-  services.openiscsi.enable = true;
-
-  boot.kernel.sysctl = {
-    "kernel.threads-max" = 8192;
-    "fs.inotify.max_user_watches" = 524288;
-    "fs.inotify.max_user_instances" = 8192;
-    "fs.inotify.max_queued_events" = 16384;
-    "fs.file-max" = 131072;
-    "user.max_inotify_instances" = 8192;
-    "user.max_inotify_watches" = 524288;
-    "vm.max_map_count" = 1048576;
-  };
-
   boot.kernelParams = [
     "cgroup_enable=cpuset"
     "cgroup_enable=memory"
