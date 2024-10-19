f7fba0090aad39c20bf1426563033eb990df8020
William Phetsinorath <william.phetsinorath@shikanime.studio>
2024-10-19 20:35:49 +0000

    Update base for None on "Refactor Nishir nodes"
    [ghstack-poisoned]

--- modules/flake/nixos.nix
+++ modules/flake/nixos.nix
@@ -1,7 +1,7 @@
 { self, inputs, withSystem, ... }:
 
 {
-  perSystem = _: {
+  flake = {
     packages = {
       nishir-raspeberry-pi4-image =
         self.nixosConfigurations.nishir-raspeberry-pi4.config.system.build.sdImage;
