f8ceedb7c2de242d7ca2a170b68c8cc895e8145f
William Phetsinorath <william.phetsinorath@shikanime.studio>
2024-10-19 20:39:05 +0000

    Remove legacy Elvengard machine
    ghstack-source-id: 6c8730bf6bc5894943f5e09bff8c31a60c605e3b
    Pull Request resolved: https://github.com/shikanime/shikanime/pull/173

--- modules/flake/nixos.nix
+++ modules/flake/nixos.nix
@@ -3,27 +3,12 @@
 {
   flake = {
     packages = {
-      x86_64-linux = {
-        elvengard-hyperv-image =
-          self.nixosConfigurations.elvengard-hyperv.config.system.build.hypervImage;
-      };
       aarch64-linux = {
         nishir-raspeberry-pi4-image =
           self.nixosConfigurations.nishir-raspeberry-pi4.config.system.build.sdImage;
       };
     };
     nixosConfigurations = {
-      elvengard-hyperv = withSystem "x86_64-linux" ({ system, ... }:
-        inputs.nixpkgs.lib.nixosSystem {
-          pkgs = import inputs.nixpkgs {
-            inherit system;
-            config.allowUnfree = true;
-          };
-          modules = [
-            ../nixos/hosts/elvengard-hyperv.nix
-            inputs.home-manager.nixosModules.home-manager
-          ];
-        });
       nishir-raspeberry-pi4 = withSystem "aarch64-linux" ({ system, ... }:
         inputs.nixpkgs.lib.nixosSystem {
           pkgs = import inputs.nixpkgs {
