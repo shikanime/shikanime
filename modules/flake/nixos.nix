fa23d95e9daef51086b7692fef7ce9e6d16d07ae
William Phetsinorath <william.phetsinorath@shikanime.studio>
2024-10-19 20:25:27 +0000

    None on "Remove legacy artifacts"
    [ghstack-poisoned]

--- modules/flake/nixos.nix
+++ modules/flake/nixos.nix
@@ -3,25 +3,12 @@
 {
   perSystem = _: {
     packages = {
-      elvengard-hyperv-image =
-        self.nixosConfigurations.elvengard-hyperv.config.system.build.hypervImage;
       nishir-raspeberry-pi4-image =
         self.nixosConfigurations.nishir-raspeberry-pi4.config.system.build.sdImage;
     };
   };
   flake = {
     nixosConfigurations = {
-      elvengard-hyperv = withSystem "x86_64-linux" ({ system, ... }:
-        inputs.nixpkgs.lib.nixosSystem {
-          pkgs = import inputs.nixpkgs {
-            inherit system;
-            config.allowUnfree = true;
-          };
-          modules = [
-            ../nixos/hosts/elvengard-hyperv.nix
-            inputs.home-manager.nixosModules.home-manager
-          ];
-        });
       nishir-raspeberry-pi4 = withSystem "aarch64-linux" ({ system, ... }:
         inputs.nixpkgs.lib.nixosSystem {
           pkgs = import inputs.nixpkgs {
