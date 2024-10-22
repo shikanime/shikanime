6cfb5209c5054ab343ba26ba2a25949b40af7bf8
William Phetsinorath <william.phetsinorath@shikanime.studio>
2024-10-22 15:41:08 +0200

    Fix tooling path

--- modules/home/profiles/go.nix
+++ modules/home/profiles/go.nix
@@ -1,4 +1,10 @@
 {
+  home.sessionPath = [
+    "${config.xdg.dataHome}/go/bin"
+  ];
+
+  home.sessionVariables.GOPATH = "${config.xdg.dataHome}/go";
+
   programs.zsh.oh-my-zsh.plugins = [
     "golang"
   ];
