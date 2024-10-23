ad0b62d3b4dcf35def54c5d238abe4902124b261
William Phetsinorath <william.phetsinorath@shikanime.studio>
2024-10-24 00:34:05 +0200

    None on "Refactor dev tooling"
    [ghstack-poisoned]

--- modules/home/profiles/go.nix
+++ modules/home/profiles/go.nix
@@ -1,13 +1,16 @@
 { config, ... }:
 
 {
-  programs.go = {
-    enable = true;
-    goPath = "${config.xdg.configHome}/go";
-    goBin = "${config.home.homeDirectory}/.local/bin";
+  home.sessionVariables = {
+    GOPATH = "${config.xdg.configHome}/go";
+    GOBIN = "${config.home.homeDirectory}/.local/bin";
   };
 
   programs.zsh.oh-my-zsh.plugins = [
     "golang"
+  ];
+
+  home.packages = [
+    pkgs.asdf-vm
   ];
 }
--- modules/home/profiles/rustup.nix
+++ modules/home/profiles/rustup.nix
@@ -5,14 +5,14 @@
     "${config.xdg.configHome}/cargo/bin"
   ];
 
-  home.packages = [
-    pkgs.rustup
-  ];
-
   home.sessionVariables = {
     CARGO_HOME = "${config.xdg.configHome}/cargo";
     RUSTUP_HOME = "${config.xdg.configHome}/rustup";
   };
+
+  home.packages = [
+    pkgs.rustup
+  ];
 
   programs.zsh.oh-my-zsh.plugins = [
     "rust"
