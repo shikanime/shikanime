0ee53746a233abb3fc0612bc2fbf27ecc0258ea4
William Phetsinorath <william.phetsinorath@shikanime.studio>
2024-10-24 20:48:06 +0200

    None on "Add ghstack land automation"
    [ghstack-poisoned]

--- delete.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for i in $(seq 148 1000); do
-  # Delete the branch locally
-  git branch -D "origin/$i"
-
-  # Push the deletion to the remote repository
-  git push origin --delete "origin/$i"
-done
