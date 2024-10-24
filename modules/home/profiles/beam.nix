7973dd26b0fd4c7e97ffc53ded0ebc89015c825d
William Phetsinorath <william.phetsinorath@shikanime.studio>
2024-10-24 11:22:51 +0200

    Remove env var set by mise

--- modules/home/profiles/beam.nix
+++ modules/home/profiles/beam.nix
@@ -1,14 +1,4 @@
-{ config, ... }:
-
 {
-  # Local programs
-  home.sessionPath = [
-    "${config.xdg.configHome}/mix/escripts"
-  ];
-
-  # Make Mix toolchain to be the XDG compliant by default
-  home.sessionVariables.MIX_XDG = 1;
-
   programs.zsh.oh-my-zsh.plugins = [
     "mix"
   ];
--- modules/home/profiles/go.nix
+++ modules/home/profiles/go.nix
@@ -1,11 +1,4 @@
-{ config, ... }:
-
 {
-  home.sessionVariables = {
-    GOPATH = "${config.xdg.configHome}/go";
-    GOBIN = "${config.home.homeDirectory}/.local/bin";
-  };
-
   programs.zsh.oh-my-zsh.plugins = [
     "golang"
   ];
