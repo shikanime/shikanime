fba883c505a62bb5f406ee37452301bd4bbc2865
William Phetsinorath <william.phetsinorath@shikanime.studio>
2024-11-02 14:45:51 +0100

    Remove containers fix
    [ghstack-poisoned]

--- modules/flake/devenv.nix
+++ modules/flake/devenv.nix
@@ -21,7 +21,6 @@
     };
     devenv.shells.default = {
       devenv.root = "/workspaces/Shikanime/shikanime"; # TODO: Wait for https://github.com/cachix/devenv/issues/1461
-      containers = pkgs.lib.mkForce { };
       languages.nix.enable = true;
       cachix = {
         enable = true;
