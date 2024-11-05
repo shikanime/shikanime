8771d34bbd595fc1f1095c56fbfa244235122f9e
William Phetsinorath <william.phetsinorath@shikanime.studio>
2024-11-05 17:49:08 +0000

    Refactor Nishir configs
    [ghstack-poisoned]

--- modules/flake/nixos.nix
+++ modules/flake/nixos.nix
@@ -3,12 +3,12 @@
 {
   perSystem = _: {
     packages = {
-      ishtar-wsl-tarballBuilder =
-        self.nixosConfigurations.ishtar-wsl.config.system.build.tarballBuilder;
-      nishir-raspeberry-pi4-remilia-sdImage =
-        self.nixosConfigurations.nishir-raspeberry-pi4-remilia.config.system.build.sdImage;
-      nishir-raspeberry-pi4-flandre-sdImage =
-        self.nixosConfigurations.nishir-raspeberry-pi4-flandre.config.system.build.sdImage;
+      ishtar-tarballBuilder =
+        self.nixosConfigurations.ishtar.config.system.build.tarballBuilder;
+      remilia-sdImage =
+        self.nixosConfigurations.remilia.config.system.build.sdImage;
+      flandre-sdImage =
+        self.nixosConfigurations.flandre.config.system.build.sdImage;
     };
   };
   flake = {
@@ -25,26 +25,26 @@
             inputs.nixos-wsl.nixosModules.default
           ];
         });
-      nishir-raspeberry-pi4-remilia = withSystem "aarch64-linux" ({ system, ... }:
+      remilia = withSystem "aarch64-linux" ({ system, ... }:
         inputs.nixpkgs.lib.nixosSystem {
           pkgs = import inputs.nixpkgs {
             inherit system;
             config.allowUnfree = true;
           };
           modules = [
-            ../nixos/hosts/nishir-raspberry-pi-4-remilia.nix
+            ../nixos/hosts/remilia.nix
             inputs.home-manager.nixosModules.home-manager
             inputs.nixos-hardware.nixosModules.raspberry-pi-4
           ];
         });
-      nishir-raspeberry-pi4-flandre = withSystem "aarch64-linux" ({ system, ... }:
+      flandre = withSystem "aarch64-linux" ({ system, ... }:
         inputs.nixpkgs.lib.nixosSystem {
           pkgs = import inputs.nixpkgs {
             inherit system;
             config.allowUnfree = true;
           };
           modules = [
-            ../nixos/hosts/nishir-raspberry-pi-4-flandre.nix
+            ../nixos/hosts/flandre.nix
             inputs.home-manager.nixosModules.home-manager
             inputs.nixos-hardware.nixosModules.raspberry-pi-4
           ];
--- modules/nixos/hosts/nishir.nix
+++ modules/nixos/hosts/nishir.nix
@@ -2,15 +2,33 @@
 
 {
   imports = [
+    "${modulesPath}/installer/sd-card/sd-image-aarch64.nix"
     "${modulesPath}/profiles/headless.nix"
     ../profiles/base.nix
     ../profiles/machine.nix
   ];
 
+  hardware.raspberry-pi."4" = {
+    fkms-3d.enable = true;
+    apply-overlays-dtmerge.enable = true;
+  };
+
+  # https://github.com/NixOS/nixpkgs/issues/154163#issuecomment-1008362877
+  nixpkgs.overlays = [
+    (_: super: {
+      makeModulesClosure = x:
+        super.makeModulesClosure (x // { allowMissing = true; });
+    })
+  ];
+
   boot.kernelParams = [
+    "8250.nr_uarts=1"
     "cgroup_enable=cpuset"
     "cgroup_enable=memory"
     "cgroup_memory=1"
+    "console=ttyAMA0,115200"
+    "console=tty1"
+    "cma=128M"
   ];
 
   boot.kernel.sysctl = {
@@ -40,6 +58,5 @@
     role = "server";
   };
 
-  # Longhorn requires open-iscsi
   services.openiscsi.enable = true;
 }
