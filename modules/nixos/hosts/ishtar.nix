1058828c4862fc2cd29a59a143f1ec00ecb980aa
William Phetsinorath <william.phetsinorath@shikanime.studio>
2024-11-06 11:05:15 +0000

    Update base for None on "Refactor Nishir configs"
    [ghstack-poisoned]

--- modules/nixos/hosts/ishtar.nix
+++ modules/nixos/hosts/ishtar.nix
@@ -1,5 +1,29 @@
 { pkgs, ... }:
 
+let
+  wsl-lib = pkgs.runCommand "wsl-lib" { } ''
+    mkdir -p "$out/lib"
+    # # we cannot just symlink the lib directory because it breaks merging with other drivers that provide the same directory
+    ln -s /usr/lib/wsl/lib/libcudadebugger.so.1 "$out/lib"
+    ln -s /usr/lib/wsl/lib/libcuda.so "$out/lib"
+    ln -s /usr/lib/wsl/lib/libcuda.so.1 "$out/lib"
+    ln -s /usr/lib/wsl/lib/libcuda.so.1.1 "$out/lib"
+    ln -s /usr/lib/wsl/lib/libd3d12core.so "$out/lib"
+    ln -s /usr/lib/wsl/lib/libd3d12.so "$out/lib"
+    ln -s /usr/lib/wsl/lib/libdxcore.so "$out/lib"
+    ln -s /usr/lib/wsl/lib/libnvcuvid.so "$out/lib"
+    ln -s /usr/lib/wsl/lib/libnvcuvid.so.1 "$out/lib"
+    ln -s /usr/lib/wsl/lib/libnvdxdlkernels.so "$out/lib"
+    ln -s /usr/lib/wsl/lib/libnvidia-encode.so "$out/lib"
+    ln -s /usr/lib/wsl/lib/libnvidia-encode.so.1 "$out/lib"
+    ln -s /usr/lib/wsl/lib/libnvidia-ml.so.1 "$out/lib"
+    ln -s /usr/lib/wsl/lib/libnvidia-opticalflow.so "$out/lib"
+    ln -s /usr/lib/wsl/lib/libnvidia-opticalflow.so.1 "$out/lib"
+    ln -s /usr/lib/wsl/lib/libnvoptix.so.1 "$out/lib"
+    ln -s /usr/lib/wsl/lib/libnvwgf2umx.so "$out/lib"
+    ln -s /usr/lib/wsl/lib/nvidia-smi "$out/lib"
+  '';
+in
 {
   imports = [
     ../profiles/base.nix
@@ -27,12 +51,10 @@
     nvidia-container-toolkit.enable = true;
   };
 
-  hardware.graphics = {
+  programs.nix-ld = {
     enable = true;
-    enable32Bit = true;
+    libraries = [ wsl-lib ];
   };
-
-  programs.nix-ld.enable = true;
 
   networking.hostName = "ishtar";
 
