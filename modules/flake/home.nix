847de4f294d520bb865ce57603bfc4740fd79151
William Phetsinorath <william.phetsinorath@shikanime.studio>
2024-11-09 03:20:36 +0100

    Update base for None on "Refactor user configs"
    [ghstack-poisoned]

--- modules/flake/home.nix
+++ modules/flake/home.nix
@@ -2,29 +2,14 @@
 
 {
   flake = {
-    packages = {
-      x86_64-linux = {
-        vscode-activationPackage =
-          self.homeConfigurations.vscode.activationPackage;
-      };
-      x86_64-darwin.shikanimedeva-kaltashar-activationPackage =
-        self.homeConfigurations."shikanimedeva@kaltashar".activationPackage;
-    };
-    homeConfigurations = {
-      "shikanimedeva@kaltashar" = withSystem "x86_64-darwin" ({ pkgs, ... }:
-        inputs.home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-          modules = [
-            ../home/users/shikanimedeva-kaltashar.nix
-          ];
-        });
-      vscode = withSystem "x86_64-linux" ({ pkgs, ... }:
-        inputs.home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-          modules = [
-            ../home/users/vscode.nix
-          ];
-        });
-    };
+    packages.x86_64-linux.vscode-activationPackage =
+      self.homeConfigurations.vscode.activationPackage;
+    homeConfigurations.vscode = withSystem "x86_64-linux" ({ pkgs, ... }:
+      inputs.home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [
+          ../home/users/vscode.nix
+        ];
+      });
   };
 }
