dd0513b6dc66765e76c092813c782923f40b826b
William Phetsinorath <william.phetsinorath@shikanime.studio>
2024-11-09 03:09:50 +0100

    Add home manager use package config
    ghstack-source-id: 09ca43d5ce1c073a43882b7b73f489ade55760e4
    Pull Request resolved: https://github.com/shikanime/shikanime/pull/199

--- modules/nixos/profiles/base.nix
+++ modules/nixos/profiles/base.nix
@@ -15,6 +15,12 @@
     flake = "github:shikanime/shikanime";
   };
 
+  # Make home-manger use packages from system
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken It‘s perfectly fine and recommended to leave
