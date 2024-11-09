51f843255ee1bd308b25113c55157e2848b9918d
William Phetsinorath <william.phetsinorath@shikanime.studio>
2024-11-09 02:41:49 +0100

    Update base for None on "Add Nix Darwin support"
    [ghstack-poisoned]

--- modules/nixos/users/shika.nix
+++ modules/nixos/users/shika.nix
@@ -16,24 +16,25 @@
     useUserPackages = true;
 
     users.shika = {
-    imports = [
-      ../../home/profiles/base.nix
-      ../../home/profiles/beam.nix
-      ../../home/profiles/cloud.nix
-      ../../home/profiles/go.nix
-      ../../home/profiles/ishtar.nix
-      ../../home/profiles/java.nix
-      ../../home/profiles/javascript.nix
-      ../../home/profiles/python.nix
-      ../../home/profiles/rustup.nix
-      ../../home/profiles/vcs.nix
-      ../../home/profiles/workstation.nix
-      ../../home/identities/shikanime.nix
-    ];
+      imports = [
+        ../../home/profiles/base.nix
+        ../../home/profiles/beam.nix
+        ../../home/profiles/cloud.nix
+        ../../home/profiles/go.nix
+        ../../home/profiles/ishtar.nix
+        ../../home/profiles/java.nix
+        ../../home/profiles/javascript.nix
+        ../../home/profiles/python.nix
+        ../../home/profiles/rustup.nix
+        ../../home/profiles/vcs.nix
+        ../../home/profiles/workstation.nix
+        ../../home/identities/shikanime.nix
+      ];
 
-    home = {
-      homeDirectory = "/home/shika";
-      username = "shika";
+      home = {
+        homeDirectory = "/home/shika";
+        username = "shika";
+      };
     };
   };
 }
