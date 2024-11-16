b4f3cb0ede34455e33f7c4f148df985fe38c9869
William Phetsinorath <william.phetsinorath@shikanime.studio>
2024-11-16 15:10:41 +0000

    Extract networking components
    [ghstack-poisoned]

--- modules/nixos/profiles/machine.nix
+++ modules/nixos/profiles/machine.nix
@@ -32,20 +32,6 @@
   # Enable dynamic timezone
   services.tzupdate.enable = true;
 
-  # Enable VPN access
-  services.tailscale = {
-    enable = true;
-    openFirewall = true;
-    useRoutingFeatures = "server";
-  };
-
-  # Enable Tailscale traffic optimizations
-  boot.kernel.sysctl = {
-    "net.core.rmem_default" = 7340032;
-    "net.ipv4.ip_forward" = 1;
-    "net.ipv6.conf.all.forwarding" = 1;
-  };
-
   # Enable Network Time Protocol
   services.ntp.enable = true;
 }
