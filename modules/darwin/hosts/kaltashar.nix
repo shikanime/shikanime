6223de07f2d542fa2c0162174fc47c102264d1db
William Phetsinorath <william.phetsinorath@shikanime.studio>
2024-12-06 01:45:55 +0100

    None on "Fix missing Pinentry"
    [ghstack-poisoned]

--- modules/darwin/hosts/kaltashar.nix
+++ modules/darwin/hosts/kaltashar.nix
@@ -5,5 +5,25 @@
     ../users/shika.nix
   ];
 
+  homebrew.casks = [
+    "appcleaner"
+    "discord"
+    "google-drive"
+    "jellyfin-media-player"
+    "microsoft-teams"
+    "obs"
+    "signal"
+    "sonarr"
+    "spotify"
+    "steam"
+    "stolendata-mpv"
+    "syncthing"
+    "tailscale"
+    "transmission"
+    "wacom-tablet"
+    "whatsapp"
+    "zoom"
+  ];
+
   networking.hostName = "kaltashar";
 }
--- modules/darwin/profiles/workstation.nix
+++ modules/darwin/profiles/workstation.nix
@@ -3,38 +3,23 @@
 
   homebrew = {
     enable = true;
+    brews = [
+      "pinentry-mac"
+    ];
     casks = [
       "android-studio"
-      "appcleaner"
       "dbeaver-community"
-      "discord"
       "firefox@developer-edition"
       "google-chrome@dev"
-      "google-drive"
-      "jellyfin-media-player"
       "microsoft-edge@dev"
-      "microsoft-teams"
       "mongodb-compass"
-      "obs"
-      "pinentry-mac"
       "rancher"
       "redisinsight"
-      "signal"
-      "sonarr"
-      "spotify"
-      "steam"
-      "stolendata-mpv"
-      "syncthing"
-      "tailscale"
-      "transmission"
       "virtualbox"
       "visual-studio-code"
-      "wacom-tablet"
-      "whatsapp"
       "wireshark"
       "xquartz"
-      "zoom"
-      ];
+    ];
   };
 
   programs.gnupg.agent = {
