24b9b76450f4eb6c8a8819d32ef321b1f2bf7e95
William Phetsinorath <william.phetsinorath@shikanime.studio>
2024-12-06 01:41:30 +0100

    Fix missing Pinentry
    [ghstack-poisoned]

--- modules/darwin/profiles/workstation.nix
+++ modules/darwin/profiles/workstation.nix
@@ -1,7 +1,41 @@
 {
   nix.linux-builder.enable = true;
 
-  homebrew.enable = true;
+  homebrew = {
+    enable = true;
+    casks = [
+      "android-studio"
+      "appcleaner"
+      "dbeaver-community"
+      "discord"
+      "firefox@developer-edition"
+      "google-chrome@dev"
+      "google-drive"
+      "jellyfin-media-player"
+      "microsoft-edge@dev"
+      "microsoft-teams"
+      "mongodb-compass"
+      "obs"
+      "pinentry-mac"
+      "rancher"
+      "redisinsight"
+      "signal"
+      "sonarr"
+      "spotify"
+      "steam"
+      "stolendata-mpv"
+      "syncthing"
+      "tailscale"
+      "transmission"
+      "virtualbox"
+      "visual-studio-code"
+      "wacom-tablet"
+      "whatsapp"
+      "wireshark"
+      "xquartz"
+      "zoom"
+      ];
+  };
 
   programs.gnupg.agent = {
     enable = true;
