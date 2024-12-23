9f9d0897b03d0920cebc7eacc2c1ceb0b5454a6c
William Phetsinorath <william.phetsinorath@shikanime.studio>
2024-12-23 16:25:38 +0100

    Add brew packages
    ghstack-source-id: 8c8287edbaf224ef0f55578365305e79e3c6bddb
    Pull Request resolved: https://github.com/shikanime/shikanime/pull/218

--- modules/darwin/hosts/kaltashar.nix
+++ modules/darwin/hosts/kaltashar.nix
@@ -1,8 +1,46 @@
+{ pkgs, ... }:
+
 {
   imports = [
     ../profiles/base.nix
     ../profiles/workstation.nix
     ../users/shika.nix
+  ];
+
+  environment.systemPackages = [
+    pkgs.pinentry_mac
+  ];
+
+  homebrew.casks = [
+    "android-studio"
+    "appcleaner"
+    "dbeaver-community"
+    "discord"
+    "firefox@developer-edition"
+    "google-chrome@dev"
+    "google-drive"
+    "jellyfin-media-player"
+    "microsoft-edge@dev"
+    "microsoft-teams"
+    "mongodb-compass"
+    "obs"
+    "rancher"
+    "redisinsight"
+    "signal"
+    "sonarr"
+    "spotify"
+    "steam"
+    "stolendata-mpv"
+    "syncthing"
+    "tailscale"
+    "transmission"
+    "virtualbox"
+    "visual-studio-code"
+    "wacom-tablet"
+    "whatsapp"
+    "wireshark"
+    "xquartz"
+    "zoom"
   ];
 
   networking.hostName = "kaltashar";
