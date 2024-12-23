5a99e3504e592652e837f4893a1587fc194bb1d8
William Phetsinorath <william.phetsinorath@shikanime.studio>
2024-12-23 21:12:33 +0100

    Remove useless nixos-wsl ref

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     identities.url = "github:shikanime/identities";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nixos-hardware.url = "github:nixos/nixos-hardware";
-    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
+    nixos-wsl.url = "github:nix-community/NixOS-WSL";
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     treefmt-nix.url = "github:numtide/treefmt-nix";
   };
