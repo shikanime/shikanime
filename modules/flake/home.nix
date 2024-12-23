161ee3e09ac34953ccf6b860d8920782b178eaed
William Phetsinorath <william.phetsinorath@shikanime.studio>
2024-12-23 20:05:06 +0100

    Remove old vscode activation script package
    ghstack-source-id: b67a163bdcd424f193f873d58602b3ac1d338033
    Pull Request resolved: https://github.com/shikanime/shikanime/pull/229

--- modules/flake/home.nix
+++ modules/flake/home.nix
@@ -1,21 +1,13 @@
-{
-  self,
-  inputs,
-  withSystem,
-  ...
-}:
+{ inputs, withSystem, ... }:
 
 {
-  flake = {
-    packages.x86_64-linux.vscode-activationPackage = self.homeConfigurations.vscode.activationPackage;
-    homeConfigurations.vscode = withSystem "x86_64-linux" (
-      { pkgs, ... }:
-      inputs.home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        modules = [
-          ../home/users/vscode.nix
-        ];
-      }
-    );
-  };
+  flake.homeConfigurations.vscode = withSystem "x86_64-linux" (
+    { pkgs, ... }:
+    inputs.home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+      modules = [
+        ../home/users/vscode.nix
+      ];
+    }
+  );
 }
