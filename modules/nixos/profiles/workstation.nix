79d4ede7658e6b686e0b9de79e3c1b0ffacec784
William Phetsinorath <william.phetsinorath@shikanime.studio>
2024-12-23 16:25:38 +0100

    Rework default shell
    ghstack-source-id: 1a42c34ced5a92842d74be9c8cab6d4f8d39d847
    Pull Request resolved: https://github.com/shikanime/shikanime/pull/222

--- modules/nixos/profiles/workstation.nix
+++ modules/nixos/profiles/workstation.nix
@@ -1,6 +1,8 @@
 { pkgs, ... }:
 
 {
+  programs.zsh.enable = true;
+
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;
--- modules/nixos/users/nishir.nix
+++ modules/nixos/users/nishir.nix
@@ -2,6 +2,7 @@
   users.users.nishir = {
     isNormalUser = true;
     home = "/home/nishir";
+    useDefaultShell = true;
     extraGroups = [ "wheel" ];
     initialHashedPassword = "$y$j9T$HB1msXB0DEq00J48zRpB20$/3rhVrTzGrv1j/cPvZ0clOM2gEe1TeylUG39wgD0C42";
   };
