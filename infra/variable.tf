e7ecc8e34f674660eac6574efec56b20e53a9ba4
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-01-17 22:22:34 +0100

    Add algorithm repo
    ghstack-source-id: ab6d834bf34beae3adf3ed946ba68af7a214cfba
    Pull Request resolved: https://github.com/shikanime/shikanime/pull/262

--- infra/variable.tf
+++ infra/variable.tf
@@ -1,7 +1,7 @@
 variable "apps" {
   type = object({
-    operator       = number
     github_actions = number
+    operator       = number
   })
   description = "Application names"
   default = {
@@ -12,14 +12,16 @@ variable "apps" {
 
 variable "repositories" {
   type = object({
-    shikanime      = string
-    ghstack_action = string
+    algorithm      = string
     features       = string
-    manifests      = string
+    ghstack_action = string
     identities     = string
+    manifests      = string
+    shikanime      = string
   })
   description = "GitHub repositories"
   default = {
+    algorithm      = "algorithm"
     features       = "features"
     ghstack_action = "ghstack-action"
     identities     = "identities"
@@ -30,14 +32,14 @@ variable "repositories" {
 
 variable "secrets" {
   type = object({
-    wakabox  = string
     nix      = string
     operator = string
+    wakabox  = string
   })
   description = "Scaleway secrets ID"
   default = {
-    wakabox  = "41ec1000-749b-429d-98e9-57d3c2e646a3"
     nix      = "f617ec38-db68-4055-92d6-5913bd4f4b42"
     operator = "993e60fe-8192-4207-bf1c-110115d4ae46"
+    wakabox  = "41ec1000-749b-429d-98e9-57d3c2e646a3"
   }
 }
