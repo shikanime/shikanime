0ed14c63c98c8de9d102d13a7d467cfafc862480
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-01-24 16:54:19 +0100

    Add Helix
    ghstack-source-id: ee4c0424c849084659061de9ef2ae650b66f20cc
    Pull Request resolved: https://github.com/shikanime/shikanime/pull/267

--- modules/home/profiles/workstation.nix
+++ modules/home/profiles/workstation.nix
@@ -40,6 +40,381 @@ with lib;
     ];
   };
 
+  programs.helix = {
+    enable = true;
+    settings = {
+      theme = "catppuccin_latte";
+
+      editor = {
+        line-number = "relative";
+        cursorline = true;
+        color-modes = true;
+
+        cursor-shape = {
+          insert = "bar";
+          normal = "block";
+          select = "underline";
+        };
+
+        indent-guides = {
+          render = true;
+        };
+      };
+
+      keys.normal = {
+        # N <=> K (K is in the QWERTY N position)
+        n = "move_char_left";
+        N = "keep_selections";
+        k = "search_next";
+        K = "search_prev";
+        # E <=> J (J now 'jumps words')
+        e = "move_line_down";
+        E = "join_selections";
+        j = "move_next_word_end";
+        J = "move_next_long_word_end";
+        # H <=> I (H is just a sideways I)
+        i = "move_char_right";
+        I = "no_op";
+        h = "insert_mode";
+        H = "insert_at_line_start";
+        # U <=> L (L is in the QWERTY U position)
+        u = "move_line_up";
+        U = "no_op";
+        l = "undo";
+        L = "redo";
+      };
+
+      keys.select = {
+        # N <=> K (K is in the QWERTY N position)
+        n = "extend_char_left";
+        N = "keep_selections";
+        k = "extend_search_next";
+        K = "extend_search_prev";
+        # E <=> J (J now 'jumps words')
+        e = "extend_visual_line_down";
+        E = "join_selections";
+        j = "extend_next_word_end";
+        J = "extend_next_long_word_end";
+        # H <=> I (H is just a sideways I)
+        i = "extend_char_right";
+        I = "no_op";
+        h = "insert_mode";
+        H = "insert_at_line_start";
+        # U <=> L (L is in the QWERTY U position)
+        u = "extend_visual_line_up";
+        U = "no_op";
+        l = "undo";
+        L = "redo";
+      };
+    };
+    themes.catppuccin_latte = {
+      attribute = "yellow";
+
+      type = "yellow";
+      "type.enum.variant" = "teal";
+
+      constructor = "sapphire";
+
+      constant = "peach";
+      "constant.character" = "teal";
+      "constant.character.escape" = "pink";
+
+      string = "green";
+      "string.regexp" = "pink";
+      "string.special" = "blue";
+      "string.special.symbol" = "red";
+
+      comment = {
+        fg = "overlay2";
+        modifiers = [ "italic" ];
+      };
+
+      variable = "text";
+      "variable.parameter" = {
+        fg = "maroon";
+        modifiers = [ "italic" ];
+      };
+      "variable.builtin" = "red";
+      "variable.other.member" = "blue";
+
+      label = "sapphire";
+
+      punctuation = "overlay2";
+      "punctuation.special" = "sky";
+
+      keyword = "mauve";
+      "keyword.control.conditional" = {
+        fg = "mauve";
+        modifiers = [ "italic" ];
+      };
+
+      operator = "sky";
+
+      function = "blue";
+      "function.macro" = "mauve";
+
+      tag = "blue";
+
+      namespace = {
+        fg = "yellow";
+        modifiers = [ "italic" ];
+      };
+
+      special = "blue";
+
+      "markup.heading.1" = "red";
+      "markup.heading.2" = "peach";
+      "markup.heading.3" = "yellow";
+      "markup.heading.4" = "green";
+      "markup.heading.5" = "blue";
+      "markup.heading.6" = "mauve";
+      "markup.list" = "teal";
+      "markup.list.unchecked" = "overlay2";
+      "markup.list.checked" = "green";
+      "markup.bold" = {
+        fg = "red";
+        modifiers = [ "bold" ];
+      };
+      "markup.italic" = {
+        fg = "red";
+        modifiers = [ "italic" ];
+      };
+      "markup.link.url" = {
+        fg = "blue";
+        modifiers = [
+          "italic"
+          "underlined"
+        ];
+      };
+      "markup.link.text" = "lavender";
+      "markup.raw" = "green";
+      "markup.quote" = "pink";
+
+      "diff.plus" = "green";
+      "diff.minus" = "red";
+      "diff.delta" = "blue";
+
+      "ui.background" = {
+        fg = "text";
+        bg = "base";
+      };
+
+      "ui.linenr" = {
+        fg = "surface1";
+      };
+      "ui.linenr.selected" = {
+        fg = "lavender";
+      };
+
+      "ui.statusline" = {
+        fg = "subtext1";
+        bg = "mantle";
+      };
+      "ui.statusline.inactive" = {
+        fg = "surface2";
+        bg = "mantle";
+      };
+      "ui.statusline.normal" = {
+        fg = "base";
+        bg = "lavender";
+        modifiers = [ "bold" ];
+      };
+      "ui.statusline.insert" = {
+        fg = "base";
+        bg = "green";
+        modifiers = [ "bold" ];
+      };
+      "ui.statusline.select" = {
+        fg = "base";
+        bg = "flamingo";
+        modifiers = [ "bold" ];
+      };
+
+      "ui.popup" = {
+        fg = "text";
+        bg = "surface0";
+      };
+      "ui.window" = {
+        fg = "crust";
+      };
+      "ui.help" = {
+        fg = "overlay2";
+        bg = "surface0";
+      };
+
+      "ui.bufferline" = {
+        fg = "subtext0";
+        bg = "mantle";
+      };
+      "ui.bufferline.active" = {
+        fg = "mauve";
+        bg = "base";
+        underline = {
+          color = "mauve";
+          style = "line";
+        };
+      };
+      "ui.bufferline.background" = {
+        bg = "crust";
+      };
+
+      "ui.text" = "text";
+      "ui.text.focus" = {
+        fg = "text";
+        bg = "surface0";
+        modifiers = [ "bold" ];
+      };
+      "ui.text.inactive" = {
+        fg = "overlay1";
+      };
+      "ui.text.directory" = {
+        fg = "blue";
+      };
+
+      "ui.virtual" = "overlay0";
+      "ui.virtual.ruler" = {
+        bg = "surface0";
+      };
+      "ui.virtual.indent-guide" = "surface0";
+      "ui.virtual.inlay-hint" = {
+        fg = "surface1";
+        bg = "mantle";
+      };
+      "ui.virtual.jump-label" = {
+        fg = "rosewater";
+        modifiers = [ "bold" ];
+      };
+
+      "ui.selection" = {
+        bg = "surface1";
+      };
+
+      "ui.cursor" = {
+        fg = "base";
+        bg = "secondary_cursor";
+      };
+      "ui.cursor.primary" = {
+        fg = "base";
+        bg = "rosewater";
+      };
+      "ui.cursor.match" = {
+        fg = "peach";
+        modifiers = [ "bold" ];
+      };
+
+      "ui.cursor.primary.normal" = {
+        fg = "base";
+        bg = "rosewater";
+      };
+      "ui.cursor.primary.insert" = {
+        fg = "base";
+        bg = "green";
+      };
+      "ui.cursor.primary.select" = {
+        fg = "base";
+        bg = "flamingo";
+      };
+
+      "ui.cursor.normal" = {
+        fg = "base";
+        bg = "secondary_cursor_normal";
+      };
+      "ui.cursor.insert" = {
+        fg = "base";
+        bg = "secondary_cursor_insert";
+      };
+      "ui.cursor.select" = {
+        fg = "base";
+        bg = "secondary_cursor";
+      };
+
+      "ui.cursorline.primary" = {
+        bg = "cursorline";
+      };
+
+      "ui.highlight" = {
+        bg = "surface1";
+        modifiers = [ "bold" ];
+      };
+
+      "ui.menu" = {
+        fg = "overlay2";
+        bg = "surface0";
+      };
+      "ui.menu.selected" = {
+        fg = "text";
+        bg = "surface1";
+        modifiers = [ "bold" ];
+      };
+
+      "diagnostic.error" = {
+        underline = {
+          color = "red";
+          style = "curl";
+        };
+      };
+      "diagnostic.warning" = {
+        underline = {
+          color = "yellow";
+          style = "curl";
+        };
+      };
+      "diagnostic.info" = {
+        underline = {
+          color = "sky";
+          style = "curl";
+        };
+      };
+      "diagnostic.hint" = {
+        underline = {
+          color = "teal";
+          style = "curl";
+        };
+      };
+      "diagnostic.unnecessary" = {
+        modifiers = [ "dim" ];
+      };
+
+      error = "red";
+      warning = "yellow";
+      info = "sky";
+      hint = "teal";
+
+      palette = {
+        rosewater = "#dc8a78";
+        flamingo = "#dd7878";
+        pink = "#ea76cb";
+        mauve = "#8839ef";
+        red = "#d20f39";
+        maroon = "#e64553";
+        peach = "#fe640b";
+        yellow = "#df8e1d";
+        green = "#40a02b";
+        teal = "#179299";
+        sky = "#04a5e5";
+        sapphire = "#209fb5";
+        blue = "#1e66f5";
+        lavender = "#7287fd";
+        text = "#4c4f69";
+        subtext1 = "#5c5f77";
+        subtext0 = "#6c6f85";
+        overlay2 = "#7c7f93";
+        overlay1 = "#8c8fa1";
+        overlay0 = "#9ca0b0";
+        surface2 = "#acb0be";
+        surface1 = "#bcc0cc";
+        surface0 = "#ccd0da";
+        base = "#eff1f5";
+        mantle = "#e6e9ef";
+        crust = "#dce0e8";
+
+        cursorline = "#e8ecf1";
+        secondary_cursor = "#e1a99d";
+        secondary_cursor_normal = "#e1a99d";
+        secondary_cursor_insert = "#74b867";
+      };
+    };
+  };
   programs.mise = {
     enable = true;
     globalConfig.settings.experimental = true;
