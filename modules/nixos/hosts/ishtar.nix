6d6c7f6729952ff807470d095212b90c5c16b751
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-02-21 17:51:07 +0000

    Add k3s
    [ghstack-poisoned]

--- modules/nixos/hosts/ishtar.nix
+++ modules/nixos/hosts/ishtar.nix
@@ -11,5 +11,21 @@
 
   networking.hostName = "ishtar";
 
+  services.k3s = {
+    enable = true;
+    role = "server";
+    extraFlags = [
+      "--cluster-cidr 10.42.0.0/16,2001:cafe:42::/56"
+      "--disable-etcd"
+      "--flannel-backend host-gw"
+      "--node-ip 100.77.66.124,fd7a:115c:a1e0::1c01:427c"
+      "--protect-kernel-defaults"
+      "--secrets-encryption"
+      "--service-cidr 10.43.0.0/16,2001:cafe:43::/112"
+      "--tls-san ishtar.taila659a.ts.net"
+    ];
+    serverAddr = "https://nishir.taila659a.ts.net:6443";
+  };
+
   wsl.defaultUser = "shika";
 }
