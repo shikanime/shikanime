7f48745ca630e80781ab6996efe686e11ceb0109
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-03-01 16:08:38 +0100

    Relax Google Cloud IAM on infra operator

--- infra/shikanime-google-cloud/sa.tf
+++ infra/shikanime-google-cloud/sa.tf
@@ -7,10 +7,6 @@ module "service_accounts" {
   display_name = "Infrastructure Operator Service Account"
 
   project_roles = [
-    "${module.project.project_id}=>roles/browser",
-    "${module.project.project_id}=>roles/iam.securityAdmin",
-    "${module.project.project_id}=>roles/iam.workloadIdentityPoolAdmin",
-    "${module.project.project_id}=>roles/serviceusage.serviceUsageAdmin",
-    "${module.project.project_id}=>roles/iam.serviceAccountAdmin",
+    "${module.project.project_id}=>roles/editor",
   ]
 }
