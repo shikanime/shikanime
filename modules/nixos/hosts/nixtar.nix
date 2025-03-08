c7c67e075340c0198f646e8b824dcb72d5950f07
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-03-08 21:10:56 +0000

    None on "Fix conflict with host networking"
    [ghstack-poisoned]

--- modules/nixos/hosts/nixtar.nix
+++ modules/nixos/hosts/nixtar.nix
@@ -18,9 +18,7 @@
     extraFlags = [
       "--cluster-cidr 10.42.0.0/16,2001:cafe:42::/56"
       "--flannel-backend host-gw"
-      "--node-ip 100.111.162.12,fd7a:115c:a1e0::7e01:a20c"
       "--service-cidr 10.43.0.0/16,2001:cafe:43::/112"
-      "--tls-san nixtar.taila659a.ts.net"
     ];
   };
 
