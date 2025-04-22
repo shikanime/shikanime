bf498cafd1c4f01eb08da9a801fa64ae565760f8
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-04-22 19:27:53 +0200

    Update base for Update on "Remove non essential machine services"
    [ghstack-poisoned]

--- hosts/nixtar/configuration.nix
+++ hosts/nixtar/configuration.nix
@@ -57,7 +57,7 @@ in
   services.passSecretService.enable = true;
 
   # Need by Docker's NVIDIA integration
-  services.xserver.videoDrivers = ["nvidia"];
+  services.xserver.videoDrivers = [ "nvidia" ];
 
   users.users.shika = {
     isNormalUser = true;
