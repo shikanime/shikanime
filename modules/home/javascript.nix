305ac616710871e0d2402b753ca9197ed87471ca
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-04-29 19:59:12 +0200

    Add Nodejs for Trae
    ghstack-source-id: 8495f82ecf76f592c5dc8198261fa1404baa3816
    Pull Request resolved: https://github.com/shikanime/shikanime/pull/352

--- modules/home/javascript.nix
+++ modules/home/javascript.nix
@@ -1,6 +1,10 @@
 { config, pkgs, ... }:
 
 {
+  home.packages = [
+    pkgs.nodejs
+  ];
+
   home.sessionVariables = {
     NPM_CONFIG_CACHE = "${config.xdg.cacheHome}/npm";
     NPM_CONFIG_USERCONFIG = "${config.xdg.configHome}/npm";
