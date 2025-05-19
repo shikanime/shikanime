3aaa72bd8e49f7279ac636e4bd53a2548105a2e1
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-05-19 15:49:39 +0200

    Fix darwin missing primary user
    [ghstack-poisoned]

--- hosts/kaltashar/darwin-configuration.nix
+++ hosts/kaltashar/darwin-configuration.nix
@@ -8,25 +8,30 @@
     ./users/shikanimedeva/home-configuration.nix
   ];
 
-  homebrew.casks = [
-    "appcleaner"
-    "discord"
-    "google-drive"
-    "jellyfin-media-player"
-    "microsoft-teams"
-    "obs"
-    "signal"
-    "spotify"
-    "steam"
-    "stolendata-mpv"
-    "syncthing"
-    "tailscale"
-    "transmission"
-    "whatsapp"
-    "zoom"
-  ];
+  homebrew = {
+    casks = [
+      "appcleaner"
+      "discord"
+      "google-drive"
+      "jellyfin-media-player"
+      "microsoft-teams"
+      "obs"
+      "signal"
+      "spotify"
+      "steam"
+      "stolendata-mpv"
+      "syncthing"
+      "tailscale"
+      "transmission"
+      "whatsapp"
+      "zoom"
+    ];
+    user = "shikanimedeva";
+  };
 
   networking.hostName = "kaltashar";
+
+  system.primaryUser = "shikanimedeva";
 
   users.users.shikanimedeva = {
     name = "shikanimedeva";
