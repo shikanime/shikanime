ff45a8989338b1e0e2a0a4ba367406c83236e7f8
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-05-20 14:09:26 +0200

    Add Telsha
    ghstack-source-id: 7dfaba7fe6a7fd8cc810f658b7f2a9117c93cf7c
    Pull Request resolved: https://github.com/shikanime/shikanime/pull/364

---
+++ hosts/telsha/darwin-configuration.nix
@@ -0,0 +1,37 @@
+{
+  imports = [
+    ../../modules/darwin/base.nix
+    ../../modules/darwin/workstation.nix
+  ];
+
+  home-manager.users.shikanimedeva.imports = [
+    ./users/shikanimedeva/home-configuration.nix
+  ];
+
+  homebrew.casks = [
+    "appcleaner"
+    "discord"
+    "google-drive"
+    "jellyfin-media-player"
+    "microsoft-teams"
+    "obs"
+    "signal"
+    "spotify"
+    "steam"
+    "stolendata-mpv"
+    "syncthing"
+    "tailscale"
+    "transmission"
+    "whatsapp"
+    "zoom"
+  ];
+
+  networking.hostName = "telsha";
+
+  system.primaryUser = "shikanimedeva";
+
+  users.users.shikanimedeva = {
+    name = "shikanimedeva";
+    home = "/Users/shikanimedeva";
+  };
+}
--- modules/flake/darwin.nix
+++ modules/flake/darwin.nix
@@ -1,19 +1,36 @@
 { inputs, withSystem, ... }:
 
 {
-  flake.darwinConfigurations.kaltashar = withSystem "x86_64-darwin" (
-    { system, ... }:
-    inputs.nix-darwin.lib.darwinSystem {
-      pkgs = import inputs.nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      };
-      modules = [
-        ../../hosts/kaltashar/darwin-configuration.nix
-        inputs.home-manager.darwinModules.home-manager
-        inputs.identities.darwinModules.kaltashar
-        inputs.sops-nix.darwinModules.sops
-      ];
-    }
-  );
+  flake.darwinConfigurations = {
+    kaltashar = withSystem "x86_64-darwin" (
+      { system, ... }:
+      inputs.nix-darwin.lib.darwinSystem {
+        pkgs = import inputs.nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+        modules = [
+          ../../hosts/kaltashar/darwin-configuration.nix
+          inputs.home-manager.darwinModules.home-manager
+          inputs.identities.darwinModules.kaltashar
+          inputs.sops-nix.darwinModules.sops
+        ];
+      }
+    );
+    telsha = withSystem "aarch64-darwin" (
+      { system, ... }:
+      inputs.nix-darwin.lib.darwinSystem {
+        pkgs = import inputs.nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+        modules = [
+          ../../hosts/telsha/darwin-configuration.nix
+          inputs.home-manager.darwinModules.home-manager
+          inputs.identities.darwinModules.telsha
+          inputs.sops-nix.darwinModules.sops
+        ];
+      }
+    );
+  };
 }
