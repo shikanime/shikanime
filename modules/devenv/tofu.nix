698caa0b6b63d6cab646b37a88ec301579296224
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-10-08 12:58:44 +0200

    Extract devenv
    ghstack-source-id: 72837bc08d4f2d0df4a623917afd0c8f0300b99d
    Pull Request resolved: https://github.com/shikanime/shikanime/pull/405

---
+++ modules/devenv/tofu.nix
@@ -0,0 +1,9 @@
+{ pkgs, ... }:
+
+{
+  languages.opentofu.enable = true;
+  git-hooks.hooks.tflint.enable = true;
+  packages = [
+    pkgs.scaleway-cli
+  ];
+}
--- modules/flake/devenv.nix
+++ modules/flake/devenv.nix
@@ -14,34 +14,16 @@
         };
         projectRootFile = "flake.nix";
         settings.global.excludes = [
-          ".devenv/*"
-          ".direnv/*"
           "*.png"
           "LICENSE"
         ];
       };
-      devenv.shells.default = {
-        cachix = {
-          enable = true;
-          push = "shikanime";
-        };
-        containers = pkgs.lib.mkForce { };
-        languages = {
-          nix.enable = true;
-          opentofu.enable = true;
-        };
-        git-hooks.hooks = {
-          actionlint.enable = true;
-          deadnix.enable = true;
-          flake-checker.enable = true;
-          tflint.enable = true;
-        };
-        packages = [
-          pkgs.scaleway-cli
-          pkgs.gh
-          pkgs.gnused
-          pkgs.marksman
-          pkgs.taplo
+      devenv= {
+        modules = [
+          ../devenv/base.nix
+        ];
+        shells.default.imports = [
+          ../devenv/tofu.nix
         ];
       };
     };
