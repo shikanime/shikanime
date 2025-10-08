78632b9baf0f247423cdda91311e1f661a7717fd
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-10-08 18:14:39 +0200

    Add GitHub Actions workflow generation
    ghstack-source-id: dea66d90173325cff018a1be64183197a65904d4
    Pull Request resolved: https://github.com/shikanime/shikanime/pull/407

--- modules/flake/devenv.nix
+++ modules/flake/devenv.nix
@@ -17,29 +17,34 @@
           "LICENSE"
         ];
       };
-      devenv.shells.default = {
-        cachix = {
-          enable = true;
-          push = "shikanime";
-        };
-        containers = pkgs.lib.mkForce { };
-        languages = {
-          nix.enable = true;
-          opentofu.enable = true;
-        };
-        git-hooks.hooks = {
-          actionlint.enable = true;
-          deadnix.enable = true;
-          flake-checker.enable = true;
-          tflint.enable = true;
-        };
-        packages = [
-          pkgs.scaleway-cli
-          pkgs.gh
-          pkgs.gnused
-          pkgs.marksman
-          pkgs.taplo
+      devenv = {
+        modules = [
+          ../devenv/github.nix
         ];
+        shells.default = {
+          cachix = {
+            enable = true;
+            push = "shikanime";
+          };
+          containers = pkgs.lib.mkForce { };
+          languages = {
+            nix.enable = true;
+            opentofu.enable = true;
+          };
+          git-hooks.hooks = {
+            actionlint.enable = true;
+            deadnix.enable = true;
+            flake-checker.enable = true;
+            tflint.enable = true;
+          };
+          packages = [
+            pkgs.scaleway-cli
+            pkgs.gh
+            pkgs.gnused
+            pkgs.marksman
+            pkgs.taplo
+          ];
+        };
       };
     };
 }
