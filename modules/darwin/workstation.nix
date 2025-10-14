070306a232eab75ca2e86f5576c32c2be6e56f96
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-10-14 17:25:07 +0000

    Remove Davinci and WhatsApp
    [ghstack-poisoned]

--- modules/darwin/workstation.nix
+++ modules/darwin/workstation.nix
@@ -48,9 +48,7 @@ with lib;
     masApps = {
       Amphetamine = 937984704;
       Bitwarden = 1352778147;
-      "DaVinci Resolve" = 571213070;
       Velja = 1607635845;
-      "WhatsApp Messenger" = 310633997;
       Xcode = 497799835;
     };
   };
