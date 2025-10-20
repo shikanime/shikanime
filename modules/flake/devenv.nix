4df5e502ead53c6ad17553d72364fbdd2625845e
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-10-20 21:20:19 +0200

    Fix missing direnv
    [ghstack-poisoned]

--- modules/flake/devenv.nix
+++ modules/flake/devenv.nix
@@ -1,3 +1,5 @@
+{ inputs, ... }:
+
 {
   perSystem =
     { pkgs, ... }:
@@ -19,48 +21,49 @@
       };
       devenv = {
         modules = [
-          ../devenv/air.nix
-          ../devenv/github.nix
-          ../devenv/gitignore.nix
+          inputs.devlib.devenvModule
         ];
-        shells.default = {
-          cachix = {
-            enable = true;
-            push = "shikanime";
-          };
-          containers = pkgs.lib.mkForce { };
-          gitignore = {
-            enable = true;
-            templates = [
-              "repo:github/gitignore/refs/heads/main/Nix.gitignore"
-              "repo:shikanime/gitignore/refs/heads/main/Devenv.gitignore"
-              "tt:jetbrains+all"
-              "tt:linux"
-              "tt:macos"
-              "tt:terraform"
-              "tt:vim"
-              "tt:visualstudiocode"
-              "tt:windows"
+        shells = {
+          default = {
+            cachix = {
+              enable = true;
+              push = "shikanime";
+            };
+            containers = pkgs.lib.mkForce { };
+            gitignore = {
+              enable = true;
+              templates = [
+                "repo:github/gitignore/refs/heads/main/Nix.gitignore"
+                "repo:shikanime/gitignore/refs/heads/main/Devenv.gitignore"
+                "tt:jetbrains+all"
+                "tt:linux"
+                "tt:macos"
+                "tt:terraform"
+                "tt:vim"
+                "tt:visualstudiocode"
+                "tt:windows"
+              ];
+            };
+            languages = {
+              nix.enable = true;
+              opentofu.enable = true;
+            };
+            git-hooks.hooks = {
+              actionlint.enable = true;
+              deadnix.enable = true;
+              flake-checker.enable = true;
+              tflint.enable = true;
+            };
+            packages = [
+              pkgs.direnv
+              pkgs.gh
+              pkgs.gnused
+              pkgs.marksman
+              pkgs.sapling
+              pkgs.scaleway-cli
+              pkgs.taplo
             ];
           };
-          languages = {
-            nix.enable = true;
-            opentofu.enable = true;
-          };
-          git-hooks.hooks = {
-            actionlint.enable = true;
-            deadnix.enable = true;
-            flake-checker.enable = true;
-            tflint.enable = true;
-          };
-          packages = [
-            pkgs.gh
-            pkgs.gnused
-            pkgs.marksman
-            pkgs.sapling
-            pkgs.scaleway-cli
-            pkgs.taplo
-          ];
         };
       };
     };
