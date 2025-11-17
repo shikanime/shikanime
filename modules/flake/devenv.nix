dd2ce60e1e3147a6468fb44e426dd3606af42a24
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-11-17 22:46:03 +0100

    Add Studio devenv module
    ghstack-source-id: 05e03a4415aed23813433aaa6a6148911260b4f5
    Pull Request resolved: https://github.com/shikanime/shikanime/pull/532

--- modules/flake/devenv.nix
+++ modules/flake/devenv.nix
@@ -1,24 +1,16 @@
+{ inputs, ... }:
+
 {
   perSystem =
     { pkgs, ... }:
     {
       devenv.shells = {
         default = {
-          cachix = {
-            enable = true;
-            push = "shikanime";
-          };
-          containers = pkgs.lib.mkForce { };
-          gitignore = {
-            enable = true;
-            enableDefaultTemplates = true;
-          };
-          github.enable = true;
-          languages = {
-            nix.enable = true;
-            opentofu.enable = true;
-            shell.enable = true;
-          };
+          imports = [
+            inputs.devlib.devenvModules.shikanime-studio
+          ];
+          cachix.push = "shikanime";
+          languages.opentofu.enable = true;
           packages = [
             pkgs.direnv
             pkgs.gh
@@ -29,14 +21,6 @@
             pkgs.skaffold
             pkgs.sops
           ];
-          treefmt = {
-            enable = true;
-            config = {
-              enableDefaultExcludes = true;
-              programs.prettier.enable = true;
-              projectRootFile = "flake.nix";
-            };
-          };
         };
         build = {
           containers = pkgs.lib.mkForce { };
