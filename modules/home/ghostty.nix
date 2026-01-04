a40982132862cfac595e4cc11521c12c3c39b3d7
William Phetsinorath <william.phetsinorath@shikanime.studio>
2026-01-04 06:45:33 +0100

    Update
    [ghstack-poisoned]

--- modules/home/ghostty.nix
+++ modules/home/ghostty.nix
@@ -7,7 +7,34 @@ with lib;
     enable = true;
     package = if pkgs.stdenv.hostPlatform.isLinux then pkgs.ghostty else pkgs.ghostty-bin;
     themes = {
-      catppuccin-latte = {
+      "Catppuccin Frappe" = {
+        palette = [
+          "0=#51576d"
+          "1=#e78284"
+          "2=#a6d189"
+          "3=#e5c890"
+          "4=#8caaee"
+          "5=#f4b8e4"
+          "6=#81c8be"
+          "7=#a5adce"
+          "8=#626880"
+          "9=#e78284"
+          "10=#a6d189"
+          "11=#e5c890"
+          "12=#8caaee"
+          "13=#f4b8e4"
+          "14=#81c8be"
+          "15=#b5bfe2"
+        ];
+        background = "303446";
+        foreground = "c6d0f5";
+        cursor-color = "f2d5cf";
+        cursor-text = "232634";
+        selection-background = "44495d";
+        selection-foreground = "c6d0f5";
+        split-divider-color = "414559";
+      };
+      "Catppuccin Latte" = {
         palette = [
           "0=#5c5f77"
           "1=#d20f39"
@@ -34,7 +61,7 @@ with lib;
       };
     };
     settings = {
-      theme = "catppuccin-latte";
+      theme = "dark:Catppuccin Frappe,light:Catppuccin Latte";
       command = "${getExe pkgs.zsh} -c ${getExe pkgs.nushell} --login";
     };
   };
