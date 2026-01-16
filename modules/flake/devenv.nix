7345da1ca4c7ad2e7a1d5a9e0b41087b21613234
William Phetsinorath <william.phetsinorath@shikanime.studio>
2026-01-16 16:32:48 +0100

    Update
    [ghstack-poisoned]

--- modules/flake/devenv.nix
+++ modules/flake/devenv.nix
@@ -11,162 +11,104 @@ with lib;
       ...
     }:
     {
-      devenv.shells = {
-        default = {
-          imports = [
-            inputs.devlib.devenvModules.docs
-            inputs.devlib.devenvModules.formats
-            inputs.devlib.devenvModules.git
-            inputs.devlib.devenvModules.github
-            inputs.devlib.devenvModules.nix
-            inputs.devlib.devenvModules.opentofu
-            inputs.devlib.devenvModules.shell
-            inputs.devlib.devenvModules.shikanime
-          ];
+      devenv.shells.default = {
+        imports = [
+          inputs.devlib.devenvModules.docs
+          inputs.devlib.devenvModules.formats
+          inputs.devlib.devenvModules.git
+          inputs.devlib.devenvModules.github
+          inputs.devlib.devenvModules.nix
+          inputs.devlib.devenvModules.opentofu
+          inputs.devlib.devenvModules.shell
+          inputs.devlib.devenvModules.shikanime
+        ];
 
-          github.workflows.wakabox = {
-            enable = true;
-            settings = {
-              name = "Wakabox";
-              on = {
-                schedule = [
-                  { cron = "0 0 * * *"; }
-                ];
-                workflow_dispatch = null;
-              };
-              jobs.wakabox = {
-                runs-on = "ubuntu-latest";
-                steps = with config.devenv.shells.default.github.lib; [
-                  {
-                    uses = "matchai/waka-box@v5.0.0";
-                    env = {
-                      GH_TOKEN = mkWorkflowRef "secrets.WAKABOX_GITHUB_TOKEN";
-                      GIST_ID = mkWorkflowRef "vars.WAKABOX_GITHUB_GIST_ID";
-                      WAKATIME_API_KEY = mkWorkflowRef "secrets.WAKATIME_API_KEY";
-                    };
-                  }
-                ];
-              };
+        github.workflows.wakabox = {
+          enable = true;
+          settings = {
+            name = "Wakabox";
+            on = {
+              schedule = [
+                { cron = "0 0 * * *"; }
+              ];
+              workflow_dispatch = null;
             };
-          };
-
-          packages = [
-            pkgs.scaleway-cli
-            pkgs.skaffold
-          ]
-          ++ lib.optional pkgs.stdenv.hostPlatform.isLinux pkgs.nixos-facter;
-
-          sops = {
-            enable = true;
-            settings = {
-              creation_rules =
-                let
-                  age = [
-                    "age139fcg32lmhxupnz5wjex44jur7v7wzf9rttp2grnjmxhukck5dmqsd9zj5" # kaltashar
-                    "age1pwl9yz4k4255a4h8qz7lafce8wxhsul0cnqwmr8528fqgujlfshshv3z3g" # telsha
-                    "age1x9v4ps90txy9mk4392uya93tyzx40te4dvns4chg5s6q8mfy03ns74jpay" # nixtar
-                  ];
-                in
-                [
-                  {
-                    path_regex = "secrets/fushi.enc.yaml";
-                    key_groups = [ { inherit age; } ];
-                  }
-                  {
-                    path_regex = "secrets/kaltashar.enc.yaml";
-                    key_groups = [
-                      {
-                        age = age ++ [
-                          "age16pkwna5hq4hh03xfj6j5uew3wq6wfr5xgqgdmg6t3a27uz2dhuqsslh56c" # host
-                        ];
-                      }
-                    ];
-                  }
-                  {
-                    path_regex = "secrets/minish.enc.yaml";
-                    key_groups = [ { inherit age; } ];
-                  }
-                  {
-                    path_regex = "secrets/nishir.enc.yaml";
-                    key_groups = [ { inherit age; } ];
-                  }
-                  {
-                    path_regex = "secrets/nixtar.enc.yaml";
-                    key_groups = [
-                      {
-                        age = age ++ [
-                          "age1um232l0h8wn9mtha2qf4f4mnf7ucjayvf5qxjvynatmasg8qg5mshekvjl" # host
-                        ];
-                      }
-                    ];
-                  }
-                  {
-                    path_regex = "secrets/telsha.enc.yaml";
-                    key_groups = [
-                      {
-                        age = age ++ [
-                          "age1eak84xcr44yfqsg843rfu2xajxsyvjwh67a630htpnd0scy7yu5szjfh8d" # host
-                        ];
-                      }
-                    ];
-                  }
-                ];
-              stores.yaml.indent = 2;
+            jobs.wakabox = {
+              runs-on = "ubuntu-latest";
+              steps = with config.devenv.shells.default.github.lib; [
+                {
+                  uses = "matchai/waka-box@v5.0.0";
+                  env = {
+                    GH_TOKEN = mkWorkflowRef "secrets.WAKABOX_GITHUB_TOKEN";
+                    GIST_ID = mkWorkflowRef "vars.WAKABOX_GITHUB_GIST_ID";
+                    WAKATIME_API_KEY = mkWorkflowRef "secrets.WAKATIME_API_KEY";
+                  };
+                }
+              ];
             };
           };
         };
-        jj-vcs = {
-          containers = mkForce { };
 
-          languages.rust.enable = true;
+        packages = [
+          pkgs.scaleway-cli
+          pkgs.skaffold
+        ]
+        ++ lib.optional pkgs.stdenv.hostPlatform.isLinux pkgs.nixos-facter;
 
-          packages = [
-            pkgs.libiconv
-          ];
-        };
-        linux = {
-          containers = mkForce { };
-
-          languages.c.enable = true;
-
-          packages = [
-            pkgs.bc
-            pkgs.bison
-            pkgs.flex
-            pkgs.gcc
-            pkgs.gnumake
-            pkgs.ncurses
-            pkgs.openssl
-            pkgs.pkg-config
-            pkgs.python3
-            pkgs.zlib
-          ]
-          ++ optional (lib.meta.availableOn pkgs.stdenv.hostPlatform pkgs.elfutils) pkgs.elfutils
-          ++ optional (lib.meta.availableOn pkgs.stdenv.hostPlatform pkgs.pahole) pkgs.pahole;
-        };
-        longhorn = {
-          containers = mkForce { };
-
-          git-hooks.hooks = {
-            golangci-lint.enable = true;
-            gotest.enable = true;
-          };
-
-          languages.go.enable = true;
-
-          packages = [
-            pkgs.docker
-            pkgs.gnumake
-            pkgs.kubectl
-            pkgs.kustomize
-          ];
-        };
-        nixos = {
-          containers = mkForce { };
-
-          packages = [
-            pkgs.nixpkgs-review
-          ];
+        sops = {
+          enable = true;
+          settings.creation_rules =
+            let
+              age = [
+                "age139fcg32lmhxupnz5wjex44jur7v7wzf9rttp2grnjmxhukck5dmqsd9zj5" # kaltashar
+                "age1pwl9yz4k4255a4h8qz7lafce8wxhsul0cnqwmr8528fqgujlfshshv3z3g" # telsha
+                "age1x9v4ps90txy9mk4392uya93tyzx40te4dvns4chg5s6q8mfy03ns74jpay" # nixtar
+              ];
+            in
+            [
+              {
+                path_regex = "secrets/fushi.enc.yaml";
+                key_groups = [ { inherit age; } ];
+              }
+              {
+                path_regex = "secrets/kaltashar.enc.yaml";
+                key_groups = [
+                  {
+                    age = age ++ [
+                      "age16pkwna5hq4hh03xfj6j5uew3wq6wfr5xgqgdmg6t3a27uz2dhuqsslh56c" # host
+                    ];
+                  }
+                ];
+              }
+              {
+                path_regex = "secrets/minish.enc.yaml";
+                key_groups = [ { inherit age; } ];
+              }
+              {
+                path_regex = "secrets/nishir.enc.yaml";
+                key_groups = [ { inherit age; } ];
+              }
+              {
+                path_regex = "secrets/nixtar.enc.yaml";
+                key_groups = [
+                  {
+                    age = age ++ [
+                      "age1um232l0h8wn9mtha2qf4f4mnf7ucjayvf5qxjvynatmasg8qg5mshekvjl" # host
+                    ];
+                  }
+                ];
+              }
+              {
+                path_regex = "secrets/telsha.enc.yaml";
+                key_groups = [
+                  {
+                    age = age ++ [
+                      "age1eak84xcr44yfqsg843rfu2xajxsyvjwh67a630htpnd0scy7yu5szjfh8d" # host
+                    ];
+                  }
+                ];
+              }
+            ];
         };
       };
     };
